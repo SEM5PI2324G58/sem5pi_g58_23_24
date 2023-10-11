71516cd385ec073ceeb6b8b6048066c0fb95b647
Marco Andrade <1211469@isep.ipp.pt>
2023-10-11 16:04:42 +0100

    Adicionado url da database ao config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ export default {
   /**
    * That long string from mlab
    */
-  databaseURL: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/test",
+  databaseURL: process.env.MONGODB_URI || "mongodb+srv://Admin:1234@sem5pi-g58-2324.iswl0yf.mongodb.net/?retryWrites=true&w=majority",
 
   /**
    * Your secret sauce
