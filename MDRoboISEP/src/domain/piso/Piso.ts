5ddab7e45e8cca2bf174376fb8f8e859fad79afc
1211396 <1211396@isep.ipp.pt>
2023-10-20 17:54:24 +0100

    [Domain] Adição dos pontos ao Piso

--- MDRoboISEP/src/domain/piso/Piso.ts
+++ MDRoboISEP/src/domain/piso/Piso.ts
@@ -4,17 +4,20 @@ import { Result } from "../../core/logic/Result";
 import { NumeroPiso } from "./NumeroPiso";
 import { DescricaoPiso } from "./DescricaoPiso";
 import { Guard } from "../../core/logic/Guard";
+import { IdPiso } from "./IdPiso";
+import { Ponto } from "../ponto/Ponto";
 
 
 interface pisoProps {
   numeroPiso: NumeroPiso;
   descricaoPiso: DescricaoPiso;
+  mapa: Ponto[][]
 }
 
 export class Piso extends AggregateRoot<pisoProps> {
 
   public returnIdPiso() : number{
-    return Number(this.id.toValue);
+    return Number(this._id.toValue());
   }
 
   public returnNumeroPiso() : number{
@@ -25,18 +28,32 @@ export class Piso extends AggregateRoot<pisoProps> {
     return this.props.descricaoPiso.props.descricao;
   }
 
-  private constructor (props : pisoProps, id?: UniqueEntityID) {
-      super(props,id)
+  public returnListaDeIdDosPontos() : number[][]{
+    let ids: number[][];
+    for (let i = 0; i < this.props.mapa.length; i++) {
+      for (let j = 0; j < this.props.mapa[i].length; j++) {
+        ids[i][j] = Number(this.props.mapa[i][j].id.toValue());
+      }
+    }  
+    return ids;
   }
 
-  public static create (props: pisoProps, id?: UniqueEntityID): Result<Piso> {
+  private constructor (props : pisoProps, id?: IdPiso) {
+      super(props,id);
+  }
+
+  public static create (props: pisoProps, id?: IdPiso): Result<Piso> {
 
     const guardedProps = [
       { argument: props.numeroPiso, argumentName: 'numeroPiso' },
       { argument: props.descricaoPiso, argumentName: 'descricaoPiso' },
+      { argument: props.mapa, argumentName: 'mapa' },
     ];
 
-    const guardResult = Guard.againstNullOrUndefined(guardedProps[0].argument,guardedProps[0].argumentName);
+    let guard1 = Guard.againstNullOrUndefined(guardedProps[0].argument,guardedProps[0].argumentName);
+    let guard2 = Guard.againstNullOrUndefined(guardedProps[2].argument,guardedProps[2].argumentName);
+
+    let guardResult = Guard.combine([guard1,guard2]);
 
     if (!guardResult.succeeded) {
       return Result.fail<Piso>(guardResult.message)
