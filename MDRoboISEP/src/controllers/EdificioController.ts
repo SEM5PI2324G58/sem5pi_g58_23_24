c702535c703381cdf6fd049ec3f2397f9416b952
Marco Andrade <1211469@isep.ipp.pt>
2023-10-21 09:44:47 +0100

    [US_150][Controller] - Criação do EdifiícioController

---
+++ MDRoboISEP/src/controllers/EdificioController.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import { Inject, Service } from 'typedi';
+import config from "../../config";
+import { Result } from "../core/logic/Result";
+import IEdificioController from './IControllers/IEdificioController';
+import IEdificioService from '../services/IServices/IEdificioService';
+import IEdificioDTO from '../dto/IEdificioDTO';
+
+@Service()
+export default class EdificioController implements IEdificioController /* TODO: extends ../core/infra/BaseController */ {
+  constructor(
+      @Inject(config.services.edificio.name) private edificioServiceInstance : IEdificioService
+  ) {}
+
+  public async criarEdificio(req: Request, res: Response, next: NextFunction) {
+    try{
+      const edificioOrError = await this.edificioServiceInstance.criarEdificio(req.body as IEdificioDTO) as Result<IEdificioDTO>;
+      if (edificioOrError.isFailure) {
+        return res.status(402).send();
+      }
+      const edificioDTO = edificioOrError.getValue();
+      return res.json( edificioDTO ).status(201);
+    }catch(e){
+      return next(e);
+    }
+  }
+}
