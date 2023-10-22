f4d247ffaff4d411e89f1022de60fd03343a1bf1
Marco Andrade <1211469@isep.ipp.pt>
2023-10-22 14:05:59 +0100

    [US_150][Domain] - Criação de métodos de return dos valores das props de Edificio

--- MDRoboISEP/src/domain/edificio/Edificio.ts
+++ MDRoboISEP/src/domain/edificio/Edificio.ts
@@ -4,7 +4,6 @@ import { Dimensao } from "./Dimensao";
 import { DescricaoEdificio } from "./DescricaoEdificio";
 import { UniqueEntityID } from "../../core/domain/UniqueEntityID";
 import { Result } from "../../core/logic/Result";
-import { Codigo } from "./Codigo";
 import { Piso } from "../piso/Piso";
 import { Guard } from "../../core/logic/Guard";
 import { Elevador } from "../elevador/Elevador";
@@ -39,5 +38,33 @@ export class Edificio extends AggregateRoot<EdificioProps> {
       const edificio = new Edificio({...props}, codigo);
       return Result.ok<Edificio>(edificio);
     }
+  }
+
+  public returnNome(): String{
+    return this.props.nome.props.nome;
+  }
+
+  public returnDescricao(): String{
+    return this.props.descricao.props.descricao;
+  }
+
+  public returnDimensaoX(): number{
+    return this.props.dimensao.props.x;
+  }
+
+  public returnDimensaoY(): number{
+    return this.props.dimensao.props.y;
+  }
+
+  public returnEdificioId(): string{
+    return this.id.toString();
+  }
+
+  public returnListaPisosId(): number[]{
+    let listaPisos: number[] = [];
+    for(let i = 0; i < this.props.listaPisos.length; i++){
+      listaPisos.push(Number(this.props.listaPisos[i].id.toValue()));
+    }
+    return listaPisos;
   }
 }
