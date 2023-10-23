3d96d19a5a43a09f2eed9140fabe626f37817c60
Ezequiel Estima <1211417@isep.ipp.pt>
2023-10-23 21:48:18 +0100

    [US270][Tests] Implementação de testes para MarcaElevador

---
+++ MDRoboISEP/tests/domain/elevador/MarcaElevador.test.ts
@@ -0,0 +1,29 @@
+import { describe } from "node:test";
+import { MarcaElevador } from '../../../src/domain/elevador/MarcaElevador'
+import * as assert from 'assert';
+
+describe('MarcaElevador', function(){
+
+    it('MarcaElevador não é criada com mais de 50 caracteres', async function () {
+        let marcaElevador = MarcaElevador.create('12345678901234567890'
+        + '1234567890123456789012345678901');
+        assert.strictEqual(marcaElevador.isFailure, true);
+    });
+
+    it('MarcaElevador é criada com 50 caracteres', async function () {
+        let marcaElevador = MarcaElevador.create('12345678901234567890'
+        + '123456789012345678901234567890');
+        assert.strictEqual(marcaElevador.isSuccess, true);
+    });
+
+    it('MarcaElevador é criada com menos de 50 caracteres', async function () {
+        let marcaElevador = MarcaElevador.create('12345678901234567890');
+        assert.strictEqual(marcaElevador.isSuccess, true);
+    });
+
+    it('MarcaElevador não é criada com carateres não alfanumericos', async function () {
+        let marcaElevador = MarcaElevador.create('12345678901234567890%%');
+        assert.strictEqual(marcaElevador.isFailure, true);
+    });
+
+});
