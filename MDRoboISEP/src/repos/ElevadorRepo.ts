23a6ee4008166d47fa1f8d04d709565d796baa51
Ezequiel Estima <1211417@isep.ipp.pt>
2023-10-24 22:56:21 +0100

    [US270][Repo] Correção do método getMaxId()

--- MDRoboISEP/src/repos/ElevadorRepo.ts
+++ MDRoboISEP/src/repos/ElevadorRepo.ts
@@ -73,11 +73,11 @@ export default class ElevadorRepo implements IElevadorRepo{
     public async getMaxId(): Promise<number> {
         try {
             var maxIdResult = await this.elevadorSchema
-                .find({}, { domainID: 1 })
+                .find({}, { domainId: 1 })
                ;
     
             if (maxIdResult && maxIdResult.length > 0) {
-                return (maxIdResult.sort((a, b) => b.domainID - a.domainID))[0].domainID;
+                return (maxIdResult.sort((a, b) => b.domainId - a.domainId))[0].domainId;
             } else {
                 return 0; 
             }
