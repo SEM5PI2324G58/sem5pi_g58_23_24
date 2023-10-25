6a4d395754c8cc206604de35038214aeb631044c
1211396 <1211396@isep.ipp.pt>
2023-10-25 22:32:23 +0100

    [US360][Application] Criação DispositivoController e DispositivoService

---
+++ MDRoboISEP/src/controllers/DispositivoController.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import { Inject, Service } from 'typedi';
+import config from "../../config";
+import { Result } from "../core/logic/Result";
+import IDispositivoController from './IControllers/IDispositivoController';
+import IDispositivoDTO from '../dto/IDispositivoDTO';
+import IDispositivoService from '../services/IServices/IDispositivoService';
+import IAdicionarRoboAFrotaDTO from '../dto/IAdicionarRoboAFrotaDTO';
+
+@Service()
+export default class DispositivoController implements IDispositivoController {
+  constructor(
+      @Inject(config.services.dispositivo.name) private dispositivoServiceInstance : IDispositivoService
+  ) {}
+
+  public async adicionarDispositivoAFrota(req: Request, res: Response, next: NextFunction) {
+    try{
+      const dispositivoOrError = await this.dispositivoServiceInstance.adicionarDispositivoAFrota(req.body as IAdicionarRoboAFrotaDTO) as Result<IDispositivoDTO>;
+      if (dispositivoOrError.isFailure) {
+        return res.json(dispositivoOrError.errorValue()).status(402).send();
+      }
+      const dispositivoDTO = dispositivoOrError.getValue();
+      return res.json( dispositivoDTO ).status(201);
+    }catch(e){
+      return next(e);
+    }
+  }
+}
---
+++ MDRoboISEP/src/services/DispositivoService.ts
@@ -0,0 +1,109 @@
+import { Container, Service, Inject } from 'typedi';
+
+import jwt from 'jsonwebtoken';
+import config from '../../config';
+import argon2 from 'argon2';
+import { randomBytes } from 'crypto';
+
+
+import { UserMap } from "../mappers/UserMap";
+import { IUserDTO } from '../dto/IUserDTO';
+
+import ICriarPisoDTO from '../dto/ICriarPisoDTO';
+
+
+import { Role } from '../domain/role';
+
+import { Result } from "../core/logic/Result";
+import IDispositivoService from './IServices/IDispositivoService';
+import IAdicionarRoboAFrotaDTO from '../dto/IAdicionarRoboAFrotaDTO';
+import IDispositivoDTO from '../dto/IDispositivoDTO';
+import ITipoDispositivoRepo from './IRepos/ITipoDispositivoRepo';
+import IDispositivoRepo from './IRepos/IDispositivoRepo';
+import { Dispositivo } from '../domain/dispositivo/Dispositivo';
+import { NumeroDeSerie } from '../domain/dispositivo/NumeroDeSerie';
+import { DescricaoDispositivo } from '../domain/dispositivo/DescricaoDispositivo';
+import { Nickname } from '../domain/dispositivo/Nickname';
+import { EstadoDispositivo } from '../domain/dispositivo/EstadoDispositivo';
+import { result } from 'lodash';
+import { CodigoDispositivo } from '../domain/dispositivo/CodigoDispositivo';
+import { DispositivoMap } from '../mappers/DispositivoMap';
+
+
+@Service()
+export default class DispositivoService implements IDispositivoService{
+  constructor(
+      @Inject(config.repos.tipoDispositivo.name) private tipoDispositivoRepo : ITipoDispositivoRepo,
+      @Inject(config.repos.dispositivo.name) private dispositivoRepo : IDispositivoRepo,
+  ) {}
+
+
+  public async adicionarDispositivoAFrota(adicionarRoboAFrotaDTO: IAdicionarRoboAFrotaDTO ): Promise<Result<IDispositivoDTO>> {
+    try {
+        const tipoDispositivo = await this.tipoDispositivoRepo.findByDomainId(adicionarRoboAFrotaDTO.tipoDispositivo);
+        let flag = !!tipoDispositivo;
+        if(!flag){
+            return Result.fail<IDispositivoDTO>("O tipo de dispositivo com o id " + adicionarRoboAFrotaDTO.tipoDispositivo +" não existe");
+        }
+        let dispositivo = await this.dispositivoRepo.findByDomainId(adicionarRoboAFrotaDTO.codigo);
+        flag = !!dispositivo;
+        if(flag){
+            return Result.fail<IDispositivoDTO>("O dispositivo com o codigo " + adicionarRoboAFrotaDTO.codigo +" já existe");
+        }
+        dispositivo = await this.dispositivoRepo.findByNickname(adicionarRoboAFrotaDTO.nickname);
+        flag = !!dispositivo;
+        if(flag){
+            return Result.fail<IDispositivoDTO>("O dispositivo com o nickname " + adicionarRoboAFrotaDTO.nickname +" já existe");
+        }
+        let listaDispositivos = await this.dispositivoRepo.findByNumeroSerie(adicionarRoboAFrotaDTO.numeroSerie);
+        for(let elemento of listaDispositivos){
+            if(elemento.returnTipoDispositivoMarca() === tipoDispositivo.returnMarca() && elemento.returnTipoDispositivoModelo() === tipoDispositivo.returnModelo()){
+                return Result.fail<IDispositivoDTO>("O dispositivo com o numero de serie " + adicionarRoboAFrotaDTO.numeroSerie +" já existe");  
+            }
+        }
+        
+        let resultado;
+        let descricao;
+        let codigoOrError = CodigoDispositivo.create(adicionarRoboAFrotaDTO.codigo);
+        let nicknameOrError = Nickname.create(adicionarRoboAFrotaDTO.nickname);
+        let numeroDeSerieOrError = NumeroDeSerie.create(adicionarRoboAFrotaDTO.numeroSerie);
+        let estadoOrError = EstadoDispositivo.create(true);
+        if (adicionarRoboAFrotaDTO.descricaoDispositivo === null || adicionarRoboAFrotaDTO.descricaoDispositivo === undefined) {
+            resultado = Result.combine([codigoOrError, nicknameOrError, numeroDeSerieOrError, estadoOrError]);
+            descricao = null;
+        }else{
+            let descricaoOrError = DescricaoDispositivo.create(adicionarRoboAFrotaDTO.descricaoDispositivo);
+            resultado = Result.combine([codigoOrError, nicknameOrError, numeroDeSerieOrError, estadoOrError, descricaoOrError]);
+            if(descricaoOrError.isSuccess){
+                descricao = descricaoOrError.getValue();
+            }
+        }
+        
+        let dispositivoOrError;
+        if(resultado.isFailure){
+            return Result.fail<IDispositivoDTO>(resultado.errorValue());
+        }else{
+            dispositivoOrError = Dispositivo.create({
+                descricaoDispositivo: descricao,
+                estado: estadoOrError.getValue(),
+                nickname: nicknameOrError.getValue(),
+                numeroSerie: numeroDeSerieOrError.getValue(),
+                tipoDeDispositivo: tipoDispositivo
+            }, codigoOrError.getValue());
+        }
+        
+        if(dispositivoOrError.isFailure){
+            return Result.fail<IDispositivoDTO>(dispositivoOrError.errorValue());
+        }else{
+            dispositivo = dispositivoOrError.getValue();
+        }
+        
+        dispositivo = await this.dispositivoRepo.save(dispositivo);
+
+        return Result.ok<IDispositivoDTO>(DispositivoMap.toDTO(dispositivo));
+    } catch (e) {
+      throw e;
+    }
+  }
+
+}
