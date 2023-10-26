09c9bc1d058584462e40bba4ea60ec5ea6e52f20
Ezequiel Estima <1211417@isep.ipp.pt>
2023-10-26 17:12:37 +0100

    [US280][Domain][Test] Implementação doas método toNorteOeste, toNorte, toOeste, e toVazio e respetivos testes

--- MDRoboISEP/tests/domain/ponto/ponto.test.ts
+++ MDRoboISEP/tests/domain/ponto/ponto.test.ts
@@ -35,5 +35,54 @@ describe('ponto domain', function () {
 
 		assert.strictEqual(ponto.returnTipoPonto(), "Elevador");
 	});
+
+	it('toNorteOeste muda o tipo de ponto para o tipo "NorteOeste"', async function () {
+		// Arrange
+		let idPonto = IdPonto.create("b.1.1").getValue();
+		let tipoPonto = TipoPonto.create(" ").getValue();
+		let coordenadas = Coordenadas.create({abscissa: 0 , ordenada: 0 }).getValue();
+		let ponto = Ponto.create({coordenadas:coordenadas,tipoPonto:tipoPonto},idPonto).getValue();
+
+		ponto.toParedeNorteOeste();
+
+		assert.strictEqual(ponto.returnTipoPonto(), "NorteOeste");
+	});
+
+	it('toNorte muda o tipo de ponto para o tipo "Norte"', async function () {
+		// Arrange
+		let idPonto = IdPonto.create("b.1.1").getValue();
+		let tipoPonto = TipoPonto.create(" ").getValue();
+		let coordenadas = Coordenadas.create({abscissa: 0 , ordenada: 0 }).getValue();
+		let ponto = Ponto.create({coordenadas:coordenadas,tipoPonto:tipoPonto},idPonto).getValue();
+
+		ponto.toParedeNorte();
+
+		assert.strictEqual(ponto.returnTipoPonto(), "Norte");
+	});
+
+	it('toOeste muda o tipo de ponto para o tipo "Oeste"', async function () {
+		// Arrange
+		let idPonto = IdPonto.create("b.1.1").getValue();
+		let tipoPonto = TipoPonto.create(" ").getValue();
+		let coordenadas = Coordenadas.create({abscissa: 0 , ordenada: 0 }).getValue();
+		let ponto = Ponto.create({coordenadas:coordenadas,tipoPonto:tipoPonto},idPonto).getValue();
+
+		ponto.toParedeOeste();
+
+		assert.strictEqual(ponto.returnTipoPonto(), "Oeste");
+	});
+
+	it('toVazio muda o tipo de ponto para o tipo "Vazio"', async function () {
+		// Arrange
+		let idPonto = IdPonto.create("b.1.1").getValue();
+		let tipoPonto = TipoPonto.create("Norte").getValue();
+		let coordenadas = Coordenadas.create({abscissa: 0 , ordenada: 0 }).getValue();
+		let ponto = Ponto.create({coordenadas:coordenadas,tipoPonto:tipoPonto},idPonto).getValue();
+
+		ponto.toVazio();
+
+		assert.strictEqual(ponto.returnTipoPonto(), " ");
+	});
+
 });
 
