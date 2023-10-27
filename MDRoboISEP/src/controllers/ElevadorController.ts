ec3bd3c97aa4e389766ceb0c1f23a80fb01d5aa6
Ezequiel Estima <1211417@isep.ipp.pt>
2023-10-27 12:38:45 +0100

    [US280][Controller] Implementação do método editarElevador no ElevadorController

--- MDRoboISEP/src/controllers/ElevadorController.ts
+++ MDRoboISEP/src/controllers/ElevadorController.ts
@@ -27,4 +27,20 @@ export default class ElevadorController implements IElevadorController{
             return next(e);
         }
     }
+
+    public async editarElevador(req: Request, res: Response, next: NextFunction) {
+        try {
+            const elevadorOrError = await this.elevadorServiceInstance.editarElevador(req.body as ICriarElevadorDTO);
+              
+            if (elevadorOrError.isFailure) {
+              return res.json(elevadorOrError.errorValue()).status(402).send();
+            }
+      
+            const criarElevadorDTO = elevadorOrError.getValue();
+            return res.json( criarElevadorDTO ).status(201);
+        }
+        catch (e) {
+            return next(e);
+        }
+    }
 }
--- MDRoboISEP/src/controllers/IControllers/IElevadorController.ts
+++ MDRoboISEP/src/controllers/IControllers/IElevadorController.ts
@@ -2,4 +2,5 @@ import { NextFunction, Request, Response } from "express";
 
 export default interface IElevadorController{
     criarElevador(req: Request, res: Response, next:NextFunction)
+    editarElevador(req: Request, res: Response, next: NextFunction)
 }
