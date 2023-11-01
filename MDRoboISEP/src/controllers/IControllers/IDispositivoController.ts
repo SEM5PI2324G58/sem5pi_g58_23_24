c2f6ea4bae9c2b4d71e163f58998ecc3cb1750e2
Ezequiel Estima <1211417@isep.ipp.pt>
2023-11-01 17:45:32 +0000

    [US380][Controller] Implementação do método listarDispositivosDaFrota

--- MDRoboISEP/src/controllers/IControllers/IDispositivoController.ts
+++ MDRoboISEP/src/controllers/IControllers/IDispositivoController.ts
@@ -2,4 +2,5 @@ import { Request, Response, NextFunction } from 'express';
 
 export default interface IDispositivoController  {
     adicionarDispositivoAFrota(req: Request, res: Response, next: NextFunction);
+    listarDispositivosDaFrota(req: Request, res: Response, next: NextFunction)
 }
