0b96385b77d8a0a512ea758e0513c575f0352cda
Marco Andrade <1211469@isep.ipp.pt>
2023-11-01 04:27:18 +0000

    [US_230][Service] - Correção na injeção de dependências do PisoService

--- MDRoboISEP/src/services/PisoService.ts
+++ MDRoboISEP/src/services/PisoService.ts
@@ -22,6 +22,7 @@ import IEditarPisoDTO from '../dto/IEditarPisoDTO';
 import IElevadorService from './IServices/IElevadorService';
 import { Edificio } from '../domain/edificio/Edificio';
 import { Elevador } from '../domain/elevador/Elevador';
+import IElevadorRepo from './IRepos/IElevadorRepo';
 
 
 
@@ -31,7 +32,7 @@ export default class PisoService implements IPisoService{
       @Inject(config.repos.piso.name) private pisoRepo : IPisoRepo,
       @Inject(config.repos.edificio.name) private edifRepo : IEdificioRepo,
       @Inject(config.repos.ponto.name) private pontoRepo : IPontoRepo,
-      @Inject(config.repos.elevador.name) private elevadorRepo : IElevadorService,
+      @Inject(config.repos.elevador.name) private elevadorRepo : IElevadorRepo,
       // @Inject(config.repos.sala.name) private salaRepo : IElevadorService,
       @Inject(config.services.elevador.name) private elevadorServiceInstance : IElevadorService
       // @Inject(config.services.sala.name) private salaServiceInstance : ISalaService
