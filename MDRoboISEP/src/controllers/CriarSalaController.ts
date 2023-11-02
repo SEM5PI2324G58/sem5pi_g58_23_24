f13f7a9be976f105e8d91b8957413c219aaea7be
David Dias <1211415@isep.ipp.pt>
2023-11-02 14:21:58 +0000

    [US310] [Controller] - Criado controlador para criação de salas, adicionada toda a lógica para conversão de dados

---
+++ MDRoboISEP/src/controllers/CriarSalaController.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import { Inject, Service } from 'typedi';
+import config from "../../config";
+import { Result } from "../core/logic/Result";
+import ISalaController from './IControllers/ISalaController';
+import ISalaService from '../services/IServices/ISalaService';
+import ISalaDTO from '../dto/ISalaDTO';
+
+@Service()
+export default class CriarSalaController implements ISalaController /* TODO: extends ../core/infra/BaseController */ {
+  constructor(
+      @Inject(config.services.passagem.name) private salaServiceInstance : ISalaService
+  ) {}
+
+  public async criarSala(req: Request, res: Response, next: NextFunction) {
+    try{
+      const salaOrError = await this.salaServiceInstance.criarSala(req.body as ISalaDTO) as Result<ISalaDTO>;
+      if (salaOrError.isFailure) {
+        return res.json(salaOrError.errorValue()).status(402).send();
+      }
+      const salaDTO = salaOrError.getValue();
+      return res.json( salaDTO ).status(201);
+    }catch(e){
+      return next(e);
+    }
+  }
+}
