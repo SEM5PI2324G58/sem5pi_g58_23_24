c91e172f25051a78ef45a5f26d40481bc1316886
Ezequiel Estima <1211417@isep.ipp.pt>
2023-11-02 23:00:17 +0000

    [US260][DTO] Criação de novos DTO's para input e output da US

---
+++ MDRoboISEP/src/dto/IListarPassagemDTO.ts
@@ -0,0 +1,7 @@
+export default interface IListarPassagemDTO {
+    id: number
+    numeroPisoA: number;
+    idPisoA: number;
+    numeroPisoB: number;
+    idPisoB: number;
+}
---
+++ MDRoboISEP/src/dto/IListarPassagensPorParDEdificioDTO.ts
@@ -0,0 +1,4 @@
+export default interface IListarPassagensPorParDEdificioDTO {
+    edificioACod : string;
+    edificioBCod : string;
+}
