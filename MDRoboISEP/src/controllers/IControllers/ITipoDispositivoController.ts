ee69136b589f8224ae4cc6aa2050776eff307561
1211396 <1211396@isep.ipp.pt>
2023-11-04 12:23:50 +0000

    [Application] Adição do delete do tipoDispositivo

--- MDRoboISEP/src/controllers/IControllers/ITipoDispositivoController.ts
+++ MDRoboISEP/src/controllers/IControllers/ITipoDispositivoController.ts
@@ -2,4 +2,5 @@ import { NextFunction, Request, Response } from "express";
 
 export default interface TipoDispositivoController {
     criarTipoDispositivo(req: Request, res: Response, next: NextFunction);
+    deleteTipoDispositivo(req: Request, res: Response, next: NextFunction);
 }
