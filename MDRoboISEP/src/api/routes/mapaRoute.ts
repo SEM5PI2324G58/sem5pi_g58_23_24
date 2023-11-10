df90a8f35f658d5009a2bed983456e449078dc5d
Marco Andrade <1211469@isep.ipp.pt>
2023-11-10 12:25:01 +0000

    [US_1120][Route] - Criação de mapaRoute

---
+++ MDRoboISEP/src/api/routes/mapaRoute.ts
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import IMapaController from "../../controllers/IControllers/IMapaController";
+import config from "../../../config";
+import Container from "typedi";
+import { Joi, celebrate } from "celebrate";
+
+const route = Router();
+
+export default (app: Router) => {
+    app.use("/mapa", route);
+
+    const ctrl = Container.get(config.controllers.mapa.name) as IMapaController;
+
+    route.patch('',
+        celebrate({
+            body: Joi.object({
+                codigoEdificio: Joi.string().required(),
+                numeroPiso: Joi.number().required(),
+                passagens : Joi.array().items(Joi.object({
+                    id : Joi.number().required(),
+                    abcissa : Joi.number().required(),
+                    ordenada : Joi.number().required(),
+                    orientacao : Joi.string().required()
+                })),
+                elevador : Joi.object({
+                    xCoord : Joi.number().required(),
+                    yCoord : Joi.number().required(),
+                    orientacao : Joi.string().required()
+                }),
+                salas : Joi.array().items(Joi.object({
+                    nome : Joi.string().required(),
+                    abcissaA : Joi.number().required(),
+                    ordenadaA : Joi.number().required(),
+                    abcissaB : Joi.number().required(),
+                    ordenadaB : Joi.number().required(),
+                    abcissaPorta : Joi.number().required(),
+                    ordenadaPorta : Joi.number().required(),
+                    orientacaoPorta : Joi.string().required()
+                }))
+            })
+        }),
+        (req, res, next) => ctrl.carregarMapa(req, res, next));
+    };
