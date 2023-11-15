2aa55a9d3e57c194ca482af7d52fbdb67e409c89
Marco Andrade <1211469@isep.ipp.pt>
2023-11-15 09:37:53 +0000

    Criação de IExportarDTO

---
+++ MDRoboISEP/src/dto/IExportarMapaDTO.ts
@@ -0,0 +1,26 @@
+export default interface ICarregarMapaDTO{
+    codigoEdificio: string,
+    numeroPiso : number,
+    matriz ?: string[][],
+    passagens ?: [{
+        id : number,
+        abcissa : number,
+        ordenada : number,
+        orientacao : string,
+    }],
+    elevador ?: {
+        xCoord : number,
+        yCoord : number,
+        orientacao: string, 
+    },
+    salas ?: [{
+        nome: string,
+        abcissaA : number,
+        ordenadaA : number,
+        abcissaB : number,
+        ordenadaB : number,
+        abcissaPorta : number,
+        ordenadaPorta : number,
+        orientacaoPorta : string,
+    }]
+}
