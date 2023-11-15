f5652471eb09a65b25fd88f64e0c15876e8f0384
Marco Andrade <1211469@isep.ipp.pt>
2023-11-15 18:08:22 +0000

    [US_1120][SPA] - Criação de Carregar Mapa

---
+++ SPA/src/dataModel/carregarMapa.ts
@@ -0,0 +1,25 @@
+export default interface CarregarMapa{
+    codigoEdificio: string,
+    numeroPiso: number,
+    passagens : [{
+        id : number,
+        abcissa : number,
+        ordenada : number,
+        orientacao : string,
+    }],
+    elevador : {
+        xCoord : number,
+        yCoord : number,
+        orientacao: string, 
+    },
+    salas: [{
+        nome: string,
+        abcissaA : number,
+        ordenadaA : number,
+        abcissaB : number,
+        ordenadaB : number,
+        abcissaPorta : number,
+        ordenadaPorta : number,
+        orientacaoPorta : string,
+    }]
+}
