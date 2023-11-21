e9929914052fc5619a2d286dda0a3ba0e9ce46e9
1211396 <1211396@isep.ipp.pt>
2023-11-21 10:54:18 +0000

    delete dos pontos no elevadoService test

--- MDRoboISEP/tests/controller/ElevadorController.test.ts
+++ MDRoboISEP/tests/controller/ElevadorController.test.ts
@@ -30,7 +30,7 @@ import IElevadorDTO from '../../src/dto/IElevadorDTO';
 describe('ElevadorController', () => {
     const sandbox = sinon.createSandbox();
     beforeEach(function() {
-        
+        this.timeout(10000);
         Container.reset();
 
         let edificioProps : any = {
