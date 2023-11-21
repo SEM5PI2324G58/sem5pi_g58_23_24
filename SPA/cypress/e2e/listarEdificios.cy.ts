75cf9f73b0ff12b33664dfd3b3cd9e5f2f773a34
Marco Andrade <1211469@isep.ipp.pt>
2023-11-21 12:27:16 +0000

    [US_1060][Test] - Criação de teste e2e

---
+++ SPA/cypress/e2e/listarEdificios.cy.ts
@@ -0,0 +1,31 @@
+describe('Edificio', () => {
+
+    beforeEach(() => {
+        
+        cy.intercept('POST', '/api/edificio').as('createEdificio');
+        cy.intercept('GET', '/api/edificio').as('getEdificio');
+        cy.visit('/criarEdificio')
+        cy.get('[name="codigo"]').type('T1');
+        cy.get('[name="nomeEdificio"]').type('Nome1');
+        cy.get('[name="descricaoEdificio"]').type('Descricao1');
+        cy.get('[name="dimensaoX"]').type('3');
+        cy.get('[name="dimensaoY"]').type('3');
+        cy.get('button').click();
+        cy.wait('@createEdificio');
+    });
+    
+    it('Listar edificios com sucesso', () => {
+        cy.visit('/listarEdificios')
+        cy.wait('@getEdificio');
+        cy.get('p-table tbody tr').contains('td', 'T1').parent('tr').within(() => {
+            cy.get('td').eq(1).should('contain.text', 'Nome1');
+            cy.get('td').eq(2).should('contain.text', 'Descricao1');
+            cy.get('td').eq(3).should('contain.text', '3');
+            cy.get('td').eq(4).should('contain.text', '3');
+        });
+    })
+
+    afterEach(() => {
+        //TODO: apagar edificio
+    });
+})
