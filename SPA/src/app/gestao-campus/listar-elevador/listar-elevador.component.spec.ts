aef1916dc356f613b9bda765c6f7770fe777df3c
Ezequiel Estima <1211417@isep.ipp.pt>
2023-11-22 22:44:09 +0000

    [SPA] Testes unitários aos componentes listar,criar e editar elevador

--- SPA/src/app/gestao-campus/listar-elevador/listar-elevador.component.spec.ts
+++ SPA/src/app/gestao-campus/listar-elevador/listar-elevador.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ElevadorService } from 'src/serviceInfo/elevador.service';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { MessageComponent } from '../../message/message.component';
+import { FormBuilder } from '@angular/forms';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { EdificioService } from 'src/serviceInfo/edificio.service';
+import { of } from 'rxjs';
 import { ListarElevadorComponent } from './listar-elevador.component';
+import { ListarElevador } from 'src/dataModel/listarElevador';
+
 
 describe('ListarElevadorComponent', () => {
   let component: ListarElevadorComponent;
@@ -8,7 +18,9 @@ describe('ListarElevadorComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ListarElevadorComponent]
+      declarations: [ListarElevadorComponent, SidebarComponent, MessageComponent],
+      imports: [HttpClientTestingModule,NgMultiSelectDropDownModule,ReactiveFormsModule],
+      providers: [FormBuilder,ElevadorService],
     });
     fixture = TestBed.createComponent(ListarElevadorComponent);
     component = fixture.componentInstance;
@@ -17,5 +29,38 @@ describe('ListarElevadorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Método listarElevador chama o método listarElevador do elevadorService', () => {
+    const cod = 'cod1';
+    const listaCodEdificios = 
+      {    id: '1',
+        marca: '1',
+        modelo: '1',
+        numeroSerie: '1',
+        descricao: '1'
+      } as unknown as ListarElevador
+;
+
+    let elevadorService = TestBed.inject(ElevadorService);
+
+    spyOn(component['elevadorService'], 'listarElevador').and.returnValue(of(listaCodEdificios));
+
+    component.myForm.controls['codigo'].setValue(cod);
+
+    component.listarElevador();
+
+    expect(elevadorService.listarElevador).toHaveBeenCalledWith(
+      cod
+    );
+  });
+
+  it('Método ngOnInit chama o método listarCodEdificios do edificioService', () => {
+    const listaCodEdificios = ["cod1", "cod2"];
+    let edificioService = TestBed.inject(EdificioService);
+    spyOn(component['edificioService'], 'listarCodEdificios').and.returnValue(of(listaCodEdificios));
+    component.ngOnInit();
+    expect(edificioService.listarCodEdificios).toHaveBeenCalled();
+    expect(component.listaCodigosEd).toEqual(listaCodEdificios);
   });
 });
