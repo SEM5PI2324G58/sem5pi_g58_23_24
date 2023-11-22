38f2367a11dd681d40588c5cc28a811531781bc3
Marco Andrade <1211469@isep.ipp.pt>
2023-11-22 18:26:01 +0000

    [US_1060][Test] - Criar teste de component

--- SPA/src/app/gestao-campus/listar-edificios/listar-edificios.component.spec.ts
+++ SPA/src/app/gestao-campus/listar-edificios/listar-edificios.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ListarEdificiosComponent } from './listar-edificios.component';
+import { MapaService } from '../../../serviceInfo/mapa.service';
+import {SidebarComponent} from '../sidebar/sidebar.component';
+import {MessageComponent} from '../../message/message.component';
+import { EdificioService } from 'src/serviceInfo/edificio.service';
+import { Edificio } from 'src/dataModel/edificio';
 
 describe('ListarEdificiosComponent', () => {
   let component: ListarEdificiosComponent;
@@ -8,7 +13,9 @@ describe('ListarEdificiosComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ListarEdificiosComponent]
+      declarations: [ListarEdificiosComponent, SidebarComponent, MessageComponent],
+      imports: [HttpClientTestingModule],
+      providers: [MapaService],
     });
     fixture = TestBed.createComponent(ListarEdificiosComponent);
     component = fixture.componentInstance;
@@ -17,5 +24,20 @@ describe('ListarEdificiosComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('', ()=>{
+    const listaEdificios = [{
+      codigo: "cod",
+      nome: "nome",
+      descricao: "descricao",
+      dimensaoX: 1,
+      dimensaoY: 1,
+    }] as Edificio[];
+    let edificioService = TestBed.inject(EdificioService);
+    spyOn(component['edificioService'], 'listarEdificios').and.returnValue(listaEdificios);
+    component.ngOnInit();
+    expect(edificioService.listarEdificios).toHaveBeenCalled();
+    expect(component.listaEdificios).toEqual(listaEdificios);
   });
 });
