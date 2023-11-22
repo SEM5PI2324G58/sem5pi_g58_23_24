2ec9184cb667fcb5b041d643ac09aaf003eac54e
1211396 <1211396@isep.ipp.pt>
2023-11-22 23:19:19 +0000

    [Test] testes dos componentes de angular

--- SPA/src/app/gestao-campus/editar-piso/editar-piso.component.spec.ts
+++ SPA/src/app/gestao-campus/editar-piso/editar-piso.component.spec.ts
@@ -1,4 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PisoService } from '../../../serviceInfo/piso.service';
+import { EdificioService } from 'src/serviceInfo/edificio.service';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { MessageComponent } from '../../message/message.component'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+import { of } from 'rxjs';
 
 import { EditarPisoComponent } from './editar-piso.component';
 
@@ -8,14 +17,49 @@ describe('EditarPisoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EditarPisoComponent]
-    });
+      declarations: [EditarPisoComponent,SidebarComponent,MessageComponent],
+      providers: [PisoService, EdificioService],
+      imports: [HttpClientTestingModule,ReactiveFormsModule]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(EditarPisoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    let edificioService = TestBed.inject(EdificioService);
+    let pisoService = TestBed.inject(PisoService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve chamar o método listarCodEdificios na inicialização do Componente (ngOnInit)', () => {
+    const mockData = ['cod1', 'cod2'];
+    spyOn(component['edificioService'], 'listarCodEdificios').and.returnValue(of(mockData));
+
+    component.ngOnInit();
+
+    expect(component.listaCodigos).toEqual(mockData);
+  });
+
+  it('deve chamar o método editarPiso do piso service no método editarPiso', () => {
+    const codigo = 'code1';
+    const numeroPiso = '1';
+    const descricaoPiso = 'Description';
+    const novoNumeroPiso = '2';
+  
+    component.myForm.controls['codigo'].setValue('code1');
+    component.myForm.controls['numeroPiso'].setValue('1');
+    component.myForm.controls['novoNumeroPiso'].setValue('2');
+    component.myForm.controls['descricaoPiso'].setValue('Description');
+
+    let pisoService = TestBed.inject(PisoService);
+    spyOn(component['pisoService'], 'editarPiso');
+    component.editarPiso();
+
+    expect(pisoService.editarPiso).toHaveBeenCalledWith(codigo, numeroPiso, novoNumeroPiso,descricaoPiso);
+  });
+
+
+
 });
