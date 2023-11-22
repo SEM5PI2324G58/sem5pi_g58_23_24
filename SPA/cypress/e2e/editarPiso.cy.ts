ba7691e97e4b5996e747090d200a3d3ed7c7d207
1211396 <1211396@isep.ipp.pt>
2023-11-22 20:47:14 +0000

    [Test] Cypress testes

---
+++ SPA/cypress/e2e/editarPiso.cy.ts
@@ -0,0 +1,63 @@
+describe('Editar Piso', () => {
+
+    beforeEach(() => {
+            
+        cy.intercept('POST', '/api/edificio').as('createEdificio');
+        cy.intercept('GET', '/api/edificio').as('getEdificio');
+        cy.intercept('POST', '/api/piso').as('createPiso');
+        cy.intercept('GET', '/api/piso?codigo=T1').as('getPisosEdificio');
+        cy.intercept('PUT', '/api/piso').as('editPiso');
+        
+        //Criar um edificio
+        cy.visit('/criarEdificio')
+        cy.get('[name="codigo"]').type('T1');
+        cy.get('[name="nomeEdificio"]').type('Nome1');
+        cy.get('[name="descricaoEdificio"]').type('Descricao1');
+        cy.get('[name="dimensaoX"]').type('3');
+        cy.get('[name="dimensaoY"]').type('3');
+        cy.get('button').click();
+        cy.wait('@createEdificio');
+
+
+        //Criar 1 pisos
+        cy.visit('/criarPiso');
+        cy.wait('@getEdificio');
+
+        cy.get('[id="codigo"]').select('T1');
+        cy.get('[name="numeroPiso"]').type('1');
+        cy.get('[name="descricaoPiso"]').type('Descricao1');
+
+        cy.get('button').click();
+        cy.wait('@createPiso');
+
+    });
+
+    it('Editar piso sucesso e2e', () => {
+        //Criar piso
+        cy.visit('/editarPiso');
+        cy.wait('@getEdificio');
+    
+        cy.get('[id="codigo"]').select('T1');
+        cy.wait('@getPisosEdificio');
+
+        cy.get('[formControlName="numeroPiso"]').select('1');
+        cy.get('[id="novoNumeroPiso"]').type('2');
+        cy.get('[id="descricaoPiso"]').type('BomDiaZe');
+    
+        cy.get('button').click();
+
+        cy.wait('@editPiso').then((interception) => {
+            expect(interception?.response?.statusCode).to.eq(200); // Adjust the status code as needed
+        });;
+
+        cy.get('[name="app-message"]').contains('Número Piso: 2, descrição: BomDiaZe atualizado com sucesso!');
+        
+    })
+
+
+    afterEach(() => {
+        cy.visit('/apagarEdificio');
+        cy.get('[name="codigo"]').type('T1');
+        cy.get('button').click();
+    });
+})
