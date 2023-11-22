d1c5478b0303f4674cb1e51c188596b39a1d26d2
Marco Andrade <1211469@isep.ipp.pt>
2023-11-22 18:25:47 +0000

    [US_1070][Test] - Criar teste de component

--- SPA/src/app/gestao-campus/editar-edificio/editar-edificio.component.spec.ts
+++ SPA/src/app/gestao-campus/editar-edificio/editar-edificio.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { EditarEdificioComponent } from './editar-edificio.component';
+import { MapaService } from '../../../serviceInfo/mapa.service';
+import {SidebarComponent} from '../sidebar/sidebar.component';
+import {MessageComponent} from '../../message/message.component';
+import { EdificioService } from 'src/serviceInfo/edificio.service';
+import { of } from 'rxjs';
+
 
 describe('EditarEdificioComponent', () => {
   let component: EditarEdificioComponent;
@@ -8,7 +14,9 @@ describe('EditarEdificioComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditarEdificioComponent]
+      declarations: [EditarEdificioComponent, SidebarComponent, MessageComponent],
+      imports: [HttpClientTestingModule],
+      providers: [MapaService],
     })
     .compileComponents();
     
@@ -19,5 +27,25 @@ describe('EditarEdificioComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Método edit chama o método editarEdificio do edificioService', () => {
+    const cod = "cod";
+    const nome = "nome";
+    const descricao = "descricao";
+
+    let edificioService = TestBed.inject(EdificioService);
+    spyOn(component['edificioService'], 'editarEdificio');
+    component.edit(cod, nome, descricao);
+    expect(edificioService.editarEdificio).toHaveBeenCalledWith(cod, nome, descricao);
+  });
+
+  it('Método ngOnInit chama o método listarCodEdificios do edificioService', () => {
+    const listaCodEdificios = ["cod1", "cod2"];
+    let edificioService = TestBed.inject(EdificioService);
+    spyOn(component['edificioService'], 'listarCodEdificios').and.returnValue(of(listaCodEdificios));
+    component.ngOnInit();
+    expect(edificioService.listarCodEdificios).toHaveBeenCalled();
+    expect(component.listaCodigos).toEqual(listaCodEdificios);
   });
 });
