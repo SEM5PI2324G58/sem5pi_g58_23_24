be081c69ada87d2b91368bf8ab6d6403dff32ce1
1211396 <1211396@isep.ipp.pt>
2023-11-23 00:01:50 +0000

    [US1090] Adição de testes no cypress

--- SPA/cypress/e2e/criarPiso.cy.ts
+++ SPA/cypress/e2e/criarPiso.cy.ts
@@ -18,6 +18,28 @@ describe('Criar piso', () => {
 
 
     });
+
+    it('Criar piso input load de pagina', () => {
+        cy.visit('/criarPiso');
+        cy.wait('@getEdificio');
+
+        cy.get('#codigo').find('option:first-child').should('have.text', 'Codigo Edificio *')        
+        cy.get('[name="numeroPiso"]').should('have.attr', 'placeholder', 'Numero do Piso *');
+        cy.get('[name="descricaoPiso"]').should('have.attr', 'placeholder','Descrição do Piso');
+
+
+    });
+
+    it('Criar piso falha sem codigo e numero selecionado selecionado', () => {
+        cy.visit('/criarPiso');
+        cy.wait('@getEdificio');
+
+        cy.get('button').click();
+
+        cy.get('[name="app-message"]').contains("ERRO: Código deve ser preenchido.");
+        cy.get('[name="app-message"]').contains("ERRO: Número do Piso deve ser preenchido.");
+ 
+    });
     
     it('Criar piso sucesso e2e', () => {
         cy.visit('/criarPiso');
@@ -36,6 +58,30 @@ describe('Criar piso', () => {
         cy.get('[name="app-message"]').contains('Piso com código: T1, número Piso: 1, descrição: Descricao1 criado com sucesso!');
 
     })
+
+    it('Criar piso falha se ja existir', () => {
+        cy.visit('/criarPiso');
+        cy.wait('@getEdificio');
+
+        cy.get('[id="codigo"]').select('T1');
+        cy.get('[name="numeroPiso"]').type('1');
+        cy.get('[name="descricaoPiso"]').type('Descricao1');
+
+        cy.get('button').click({ multiple: true });
+        cy.wait('@createPiso').then((interception) => {
+            expect(interception?.response?.statusCode).to.eq(201); // Adjust the status code as needed
+        });;
+
+        cy.get('button').click({ multiple: true });
+        cy.wait('@createPiso').then((interception) => {
+            expect(interception?.response?.statusCode).to.eq(400); // Adjust the status code as needed
+        });;
+
+        
+        cy.get('[name="app-message"]').contains('Criar Piso falhou: O piso numero 1 já existe');
+
+
+    });
 
     afterEach(() => {
         cy.visit('/apagarEdificio');
