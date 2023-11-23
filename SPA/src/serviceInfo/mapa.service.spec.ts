488a07dc213930f302b173a9b1649fa3a36c8f6d
Marco Andrade <1211469@isep.ipp.pt>
2023-11-23 15:26:55 +0000

    Criar testes de serviço de mapa SPA

---
+++ SPA/src/serviceInfo/mapa.service.spec.ts
@@ -0,0 +1,77 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import CarregarMapa from 'src/dataModel/carregarMapa';
+import { of } from 'rxjs';
+
+
+import { MapaService } from './mapa.service';
+import ExportarMapa from 'src/dataModel/exportarMapa';
+
+describe('MapaService', () => {
+  let service: MapaService;
+  let httpClient: HttpClient;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+    service = TestBed.inject(MapaService);
+    httpClient = TestBed.inject(HttpClient);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('Método carregarMapa chama o método patch do HttpClient', () => {
+    const testData: CarregarMapa = {
+      codigoEdificio: "cod",
+      numeroPiso: 1,
+    } as CarregarMapa;
+
+    const patchSpy = spyOn(httpClient, 'patch').and.returnValue(of(testData));
+    service.carregarMapa(testData);
+    expect(patchSpy).toHaveBeenCalledWith('http://localhost:4000/api/mapa', testData, service.httpOptions);
+  });
+
+  it('Método exportarMapa chama o método get do HttpClient', () => {
+    let testData : ExportarMapa = {
+      texturaChao: "texturaChao",
+      texturaParede: "texturaParede",
+      modeloPorta: "modelo",
+      modeloElevador: "modelo",
+      codigoEdificio: "cod1",
+      numeroPiso : 1,
+      matriz : [["Norte","Norte"],["Oeste","Oeste"]],
+      elevador: {
+          xCoord: 1,
+          yCoord: 1,
+          orientacao: "Norte"
+      },
+      passagens : [{
+          id: 1,
+          abcissaA: 1,
+          ordenadaA: 2,
+          abcissaB: 1,
+          ordenadaB: 2,
+          orientacao: "Norte"
+      }],
+      portas : [{
+          abcissa : 1,
+          ordenada: 1,
+          orientacao: "Norte"
+      }],
+      posicaoInicialRobo : {
+          x: 1,
+          y: 2,
+      }
+    };
+
+    let params = new HttpParams().set('codEdificio', testData.codigoEdificio);
+    params = params.append('numPiso', testData.numeroPiso.toString());
+
+    const getSpy = spyOn(httpClient, 'get').and.returnValue(of(testData));
+    service.exportarMapa(testData.codigoEdificio, testData.numeroPiso);
+    expect(getSpy).toHaveBeenCalledWith("http://localhost:4000/api/mapa", {params: params, headers: service.httpOptions.headers});
+  });
+});
