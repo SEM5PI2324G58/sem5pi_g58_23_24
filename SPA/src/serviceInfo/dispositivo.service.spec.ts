2caf896af08577d631a10d5576fcde23a8bf5bf5
1211396 <1211396@isep.ipp.pt>
2023-11-23 23:58:55 +0000

    Adição dos testes do dispositivoService

--- SPA/src/serviceInfo/dispositivo.service.spec.ts
+++ SPA/src/serviceInfo/dispositivo.service.spec.ts
@@ -41,4 +41,92 @@ describe('DispositivoServiceService', () => {
     service.listarDispositivosFrota();
     expect(getSpy).toHaveBeenCalled();
   });
+
+  it('Método adicionarDispositivoAFrota  chama o método post do HttpClient', () => {
+  const testInsertData = {
+    codigo: "A",
+    nickname: "nick",
+    idTipoDispositivo: "1",
+    numeroSerie: "123",
+    descricao: "descricao"
+  };
+
+  const testExpectedData = {
+    codigo: "A",
+    nickname: "nick",
+    tipoDispositivo: 1,
+    numeroSerie: "123",
+    descricaoDispositivo: "descricao"
+  };
+  
+  const postSpy = spyOn(httpClient, 'post').and.returnValue(of(testExpectedData));
+
+  service.adicionarDispositivoAFrota(testInsertData.codigo, testInsertData.nickname, testInsertData.idTipoDispositivo, testInsertData.numeroSerie, testInsertData.descricao);
+
+  expect(postSpy).toHaveBeenCalledWith(dispositivoUrl,testExpectedData,service.httpOptions);
+  });
+
+  it('Método adicionarDispositivoAFrota não chama o método post do HttpClient se o codigo for vazio', () => {
+    const testInsertData = {
+      codigo: "",
+      nickname: "nick",
+      idTipoDispositivo: "1",
+      numeroSerie: "123",
+      descricao: "descricao"
+    };
+  
+    const postSpy = spyOn(httpClient, 'post');
+  
+    service.adicionarDispositivoAFrota(testInsertData.codigo, testInsertData.nickname, testInsertData.idTipoDispositivo, testInsertData.numeroSerie, testInsertData.descricao);
+  
+    expect(postSpy).not.toHaveBeenCalled();
+    });
+
+    it('Método adicionarDispositivoAFrota não chama o método post do HttpClient se o nickname for vazio', () => {
+      const testInsertData = {
+        codigo: "W",
+        nickname: "",
+        idTipoDispositivo: "1",
+        numeroSerie: "123",
+        descricao: "descricao"
+      };
+    
+      const postSpy = spyOn(httpClient, 'post');
+    
+      service.adicionarDispositivoAFrota(testInsertData.codigo, testInsertData.nickname, testInsertData.idTipoDispositivo, testInsertData.numeroSerie, testInsertData.descricao);
+    
+      expect(postSpy).not.toHaveBeenCalled();
+    });
+
+    it('Método adicionarDispositivoAFrota não chama o método post do HttpClient se o idTipoDispositivo for vazio', () => {
+      const testInsertData = {
+        codigo: "W",
+        nickname: "nick",
+        idTipoDispositivo: "",
+        numeroSerie: "123",
+        descricao: "descricao"
+      };
+    
+      const postSpy = spyOn(httpClient, 'post');
+    
+      service.adicionarDispositivoAFrota(testInsertData.codigo, testInsertData.nickname, testInsertData.idTipoDispositivo, testInsertData.numeroSerie, testInsertData.descricao);
+    
+      expect(postSpy).not.toHaveBeenCalled();
+    });
+
+    it('Método adicionarDispositivoAFrota não chama o método post do HttpClient se o numeroSerie for vazio', () => {
+      const testInsertData = {
+        codigo: "W",
+        nickname: "nick",
+        idTipoDispositivo: "1",
+        numeroSerie: "",
+        descricao: "descricao"
+      };
+    
+      const postSpy = spyOn(httpClient, 'post');
+    
+      service.adicionarDispositivoAFrota(testInsertData.codigo, testInsertData.nickname, testInsertData.idTipoDispositivo, testInsertData.numeroSerie, testInsertData.descricao);
+    
+      expect(postSpy).not.toHaveBeenCalled();
+    });
 });
