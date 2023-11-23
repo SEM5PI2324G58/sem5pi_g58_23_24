a79481d9c8bcd2de4da24ff0c4f0bdf0bd64786b
Marco Andrade <1211469@isep.ipp.pt>
2023-11-23 15:24:11 +0000

    Criar testes de service de tipo robo SPA

--- SPA/src/serviceInfo/tipo-robo.service.spec.ts
+++ SPA/src/serviceInfo/tipo-robo.service.spec.ts
@@ -1,16 +1,60 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { TipoRobo } from 'src/dataModel/tipoRobo';
+import { of } from 'rxjs';
 
 import { TipoRoboService } from './tipo-robo.service';
 
 describe('TipoRoboService', () => {
   let service: TipoRoboService;
-
+  let httpClient: HttpClient;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(TipoRoboService);
+    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Método criarTipoRobo chama o método post do HttpClient', () => {
+    const testData: TipoRobo = {idTipoDispositivo: 1, tipoTarefa: ["Tarefa"], marca: "marca", modelo: "modelo"};
+    const testDataInput = {tipoTarefa: ["Tarefa"], marca: "marca", modelo: "modelo"}
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(testData));
+
+    service.criarTipoRobo(testData.tipoTarefa, testData.marca, testData.modelo);
+    
+    expect(postSpy).toHaveBeenCalledWith('http://localhost:4000/api/tipoDispositivo', testDataInput, service.httpOptions);
+  });
+
+  it('Método criarTipoRobo com marca vazia não chama o método post do HttpClient', () => {
+    const testData: TipoRobo = {idTipoDispositivo: 1, tipoTarefa: ["Tarefa"], marca: "", modelo: "modelo"};
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(testData));
+
+    service.criarTipoRobo(testData.tipoTarefa, testData.marca, testData.modelo);
+    
+    expect(postSpy).not.toHaveBeenCalled();
+  });
+
+  it('Método criarTipoRobo com modelo vazio não chama o método post do HttpClient', () => {
+    const testData: TipoRobo = {idTipoDispositivo: 1, tipoTarefa: ["Tarefa"], marca: "marca", modelo: ""};
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(testData));
+
+    service.criarTipoRobo(testData.tipoTarefa, testData.marca, testData.modelo);
+    
+    expect(postSpy).not.toHaveBeenCalled();
+  });
+
+  it('Método criarTipoRobo com tipoTarefa vazio não chama o método post do HttpClient', () => {
+    const testData: TipoRobo = {idTipoDispositivo: 1, tipoTarefa: [], marca: "marca", modelo: "modelo"};
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(testData));
+
+    service.criarTipoRobo(testData.tipoTarefa, testData.marca, testData.modelo);
+    
+    expect(postSpy).not.toHaveBeenCalled();
   });
 });
