68bc51c168285dadf39d85515cbdbe93052dc0a3
David Dias <1211415@isep.ipp.pt>
2023-11-26 19:44:21 +0000

    Integracao planeamento ao servidor do DEI

--- MDRoboISEP/src/services/ImplServices/EdificioService.ts
+++ MDRoboISEP/src/services/ImplServices/EdificioService.ts
@@ -241,7 +241,7 @@ export default class EdificioService implements IEdificioService {
     try {
       // Dynamic import of 'fetch'
       const { default: fetch } = await import('node-fetch');
-      const url = "http://127.0.0.1:8000/caminho/pontos_piso"
+      const url = "http://10.9.11.58:8000/caminho/pontos_piso"
       // Faça a requisição HTTP POST
       const response = await fetch(url, {
         method: 'POST',
