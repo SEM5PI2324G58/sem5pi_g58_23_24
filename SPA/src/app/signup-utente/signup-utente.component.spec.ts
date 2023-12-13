b7917266ce96f3ff769237e15c32ea4d47e1c46b
1211396 <1211396@isep.ipp.pt>
2023-12-13 17:30:56 +0000

    [US20.1][Angular] Adição dos testes de componente e do serviço

---
+++ SPA/src/app/signup-utente/signup-utente.component.spec.ts
@@ -0,0 +1,46 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MessageComponent } from '../message/message.component'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { of } from 'rxjs';
+import { AuthService } from 'src/serviceInfo/auth.service';
+import { SignupUtenteComponent } from './signup-utente.component';
+
+describe('SignupUtenteComponent', () => {
+  let component: SignupUtenteComponent;
+  let fixture: ComponentFixture<SignupUtenteComponent>;
+
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SignupUtenteComponent,MessageComponent],
+      providers: [AuthService],
+      imports: [HttpClientTestingModule]
+    }).compileComponents();
+    fixture = TestBed.createComponent(SignupUtenteComponent);
+    component = fixture.componentInstance
+  });
+
+  
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('deve chamar o método signupUtente do authService método signupUtente', () => {
+    
+    const name = 'name';
+    const email = 'email@isep.ipp.pt';
+    const password = 'Password10@';
+    const nif = '123456789';
+    const telefone = '123456789';
+
+    let authService = TestBed.inject(AuthService);
+
+    spyOn(component['authService'], 'signupUtente');
+    component.signupUtente(name,email,telefone,nif,password);
+
+    expect(authService.signupUtente).toHaveBeenCalledWith(name,email,telefone,nif,password);
+  });
+
+});
