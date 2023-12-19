09c5cd187b1e4f7e6b06106898c13dbc20285a58
David Dias <1211415@isep.ipp.pt>
2023-12-19 09:51:49 +0000

    [US10][Test] - Adição Integração Serviço e Controller

--- auth/test/service/userService.test.ts
+++ auth/test/service/userService.test.ts
@@ -12,34 +12,99 @@ import { UserNumeroContribuinte } from '../../src/domain/user/userNumeroContribu
 import { UserName } from '../../src/domain/user/userName';
 import { UserTelefone } from '../../src/domain/user/userTelefone';
 import { ISignupUtenteDTO } from '../../src/dto/ISignupUtenteDTO';
-import UserService  from '../../src/services/ImplServices/userService';
+import UserService from '../../src/services/ImplServices/userService';
 import IUserRepo from '../../src/services/IRepos/IUserRepo';
 
 
 import "reflect-metadata";
 
 import 'mocha';
+import { IUserDTO } from "../../src/dto/IUserDTO";
 
 
 describe('User Service ', () => {
 
     const sandbox = sinon.createSandbox();
-    
-    beforeEach(function() {
+
+    beforeEach(function () {
         Container.reset();
         this.timeout(10000);
         let dispositivoSchemaInstance = require('../../src/persistence/schemas/userSchema').default;
         Container.set("userSchema", dispositivoSchemaInstance);
-       
+
         let userRepoClass = require('../../src/repos/userRepo').default;
         let userRepoInstance = Container.get(userRepoClass);
         Container.set("UserRepo", userRepoInstance);
 
     });
-    
+
     afterEach(() => {
         sinon.restore();
         sandbox.restore();
+    });
+
+    it('signup com sucesso', async () => {
+
+        let body = {
+            "name": "Marco Antonio",
+            "email": "marco@isep.ipp.pt",
+            "telefone": "914231321",
+            "nif": "321123567",
+            "password": "Password10@",
+            "estado": "aceito",
+            "role": "admin"
+        };
+
+
+
+        const hashedPassword = await UserPassword.create({ value: body.password })
+        let userProps = {
+            name: UserName.create(body.name).getValue(),
+            telefone: UserTelefone.create(body.telefone).getValue(),
+            nif: UserNumeroContribuinte.create(body.nif).getValue(),
+            password: hashedPassword.getValue(),
+            role: Role.create("admin").getValue(),
+            estado: UserEstado.create("aceito").getValue()
+        };
+        let user = User.create(userProps, UserEmail.create(body.email).getValue());
+        let userRepoInstance = Container.get("UserRepo");
+        sinon.stub(userRepoInstance, "findByEmail").returns(Promise.resolve(null));
+        sinon.stub(userRepoInstance, "save").returns(Promise.resolve(user));
+        const tipoDispositivoService = new UserService(userRepoInstance as IUserRepo);
+        let answer = await tipoDispositivoService.SignUp(body as IUserDTO);
+        expect("Conta criada com sucesso!").to.equal(answer.getValue());
+    });
+
+
+    it('signup falha porque o email já existe', async () => {
+
+        let body = {
+            "name": "Marco Antonio",
+            "email": "marco@isep.ipp.pt",
+            "telefone": "914231321",
+            "nif": "321123567",
+            "password": "Password10@",
+            "estado": "aceito",
+            "role": "admin"
+        };
+
+
+
+        const hashedPassword = await UserPassword.create({ value: body.password })
+        let userProps = {
+            name: UserName.create(body.name).getValue(),
+            telefone: UserTelefone.create(body.telefone).getValue(),
+            nif: UserNumeroContribuinte.create(body.nif).getValue(),
+            password: hashedPassword.getValue(),
+            role: Role.create("admin").getValue(),
+            estado: UserEstado.create("aceito").getValue()
+        };
+        let user = User.create(userProps, UserEmail.create(body.email).getValue());
+        let userRepoInstance = Container.get("UserRepo");
+        sinon.stub(userRepoInstance, "findByEmail").returns(user);
+        const tipoDispositivoService = new UserService(userRepoInstance as IUserRepo);
+        let answer = await tipoDispositivoService.SignUp(body as IUserDTO);
+        expect("Utilizador já existe com email marco@isep.ipp.pt").to.equal(answer.errorValue());
     });
 
 
@@ -74,7 +139,7 @@ describe('User Service ', () => {
 
         let answer = await tipoDispositivoService.signupUtente(body as ISignupUtenteDTO);
         expect("Conta criada com sucesso!").to.equal(answer.getValue());
-        
+
     });
 
     it('signupUtente falha porque o email já existe', async () => {
@@ -107,7 +172,7 @@ describe('User Service ', () => {
 
         let answer = await tipoDispositivoService.signupUtente(body as ISignupUtenteDTO);
         expect("Já existe um utilizador com esse email").to.equal(answer.errorValue());
-        
+
     });
 
 });
