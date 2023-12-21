b2340e409e14f8b00af6bb84eb802468d017640a
Ezequiel Estima <1211417@isep.ipp.pt>
2023-12-21 18:41:14 +0000

    [US460.2][Testes] Criação de testes para inexistência de parâmetros no TarefaService

--- MDTarefasTest/Unit/Services/TarefaServiceTest.cs
+++ MDTarefasTest/Unit/Services/TarefaServiceTest.cs
@@ -19,4 +19,33 @@ public class TarefaServiceTest {
         Assert.Equal("Tipo de tarefa inválido", exception.Message);
     }
 
+
+    [Fact]
+    public async void ensureMissingVigilanciaParametersThrowsException() {
+        var tarefaRepo = new Mock<TarefaRepo>();
+        var tarefaService = new TarefaService(tarefaRepo.Object);
+
+        var criarTarefaDTO = new CriarTarefaDTO();
+        criarTarefaDTO.TipoTarefa = "vigilancia";
+
+        async Task ActAsync() => await tarefaService.criarTarefa(criarTarefaDTO);
+
+        var exception = await Assert.ThrowsAsync<BusinessRuleValidationException>(ActAsync);
+
+        Assert.Equal("Tarefa de vigilância necessita de um contacto (nome e nº de telefone), código de edifício e número de piso", exception.Message);
+    }
+    [Fact]
+    public async void ensureMissingPickUpDeliveryParametersThrowsException() {
+        var tarefaRepo = new Mock<TarefaRepo>();
+        var tarefaService = new TarefaService(tarefaRepo.Object);
+
+        var criarTarefaDTO = new CriarTarefaDTO();
+        criarTarefaDTO.TipoTarefa = "PICkUPDELIVERY";
+
+        async Task ActAsync() => await tarefaService.criarTarefa(criarTarefaDTO);
+
+        var exception = await Assert.ThrowsAsync<BusinessRuleValidationException>(ActAsync);
+
+        Assert.Equal("Tarefa de pick up and delivery necessita de um código de confirmação, descrição de entrega, contactos (nome e nº de telefone) de pick up e delivery, sala inicial e sala final", exception.Message);
+    }
 }
