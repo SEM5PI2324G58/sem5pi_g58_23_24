06956bfaa81d4334745cb4eb68db1c3bd5e32059
1211396 <1211396@isep.ipp.pt>
2023-12-22 14:31:21 +0000

    Testes do controller e de integração do MDtarefas para o método GetTarefasPendentes

--- MDTarefasTest/Unit/Controller/TarefaControllerTest.cs
+++ MDTarefasTest/Unit/Controller/TarefaControllerTest.cs
@@ -127,5 +127,106 @@ namespace MDTarefasTest.Unit.Controller
             Assert.Equal("A201", dtoRes.SalaInicial);
             Assert.Equal("A202", dtoRes.SalaFinal);
         }
+
+        
+        [Fact]
+        public async void listarTarefasPendentesDevolveNotFoundCasoNaoExistamTarefas(){
+            var serviceMock = new Mock<ITarefaService>();
+            serviceMock.Setup(service => service.listarTarefasPendentes())
+                .ReturnsAsync(new List<TarefaDTO>());
+
+            var controller = new TarefaController(serviceMock.Object);
+
+            var res = await controller.GetTarefasPendentes();
+
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(res.Result);
+            Assert.Equal(404, notFoundResult.StatusCode);
+            Assert.Equal("Não existem tarefas pendentes", notFoundResult.Value);
+        }
+
+        [Fact]
+        public async void listarTarefasPendentesDevolveTarefasPendentes(){
+            var serviceMock = new Mock<ITarefaService>();
+
+            TarefaDTO tarefaDTO = new TarefaDTO();
+            tarefaDTO.Id = "id";
+            tarefaDTO.TipoTarefa = "PickUpDelivery";
+            tarefaDTO.CodConfirmacao = "12345";
+            tarefaDTO.DescricaoEntrega = "DESC";
+            tarefaDTO.NomePickUp = "NOMEPCIKUP";
+            tarefaDTO.NumeroPickUp = "123456789";
+            tarefaDTO.NomeDelivery = "NOMEDELIVERY";
+            tarefaDTO.NumeroDelivery = "987654321";
+            tarefaDTO.SalaInicial = "A201";
+            tarefaDTO.SalaFinal = "A202";
+            tarefaDTO.PercursoString = "[cel(a1,1,1),cel(a1,2,2)]";
+
+
+            serviceMock.Setup(service => service.listarTarefasPendentes())
+                .ReturnsAsync(new List<TarefaDTO>(){tarefaDTO});
+
+            var controller = new TarefaController(serviceMock.Object);
+
+            var res = await controller.GetTarefasPendentes();
+
+            var okResult = Assert.IsType<OkObjectResult>(res.Result);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.Equal(new List<TarefaDTO>(){tarefaDTO}, okResult.Value);
+        }
+
+        [Fact]
+        public async void controllerServiceListarTarefasPendentesDevolveTarefasPendentes(){
+            var tarefaRepoMock = new Mock<ITarefaRepo>();
+            var httpClient = new HttpClient();
+
+            Tarefa tarefa = new PickUpDelivery(
+                "12345","DESC",
+                "123456789","NOMEPCIKUP",
+                "987654321","NOMEDELIVERY",
+                "A201","A202",
+                "[cel(a1,1,1),cel(a1,2,2)]","emailplaceholder","id",""
+            );
+
+            TarefaDTO tarefaDTO = new TarefaDTO();
+            tarefaDTO.Id = "id";
+            tarefaDTO.TipoTarefa = "PickUpDelivery";
+            tarefaDTO.CodConfirmacao = "12345";
+            tarefaDTO.DescricaoEntrega = "DESC";
+            tarefaDTO.NomePickUp = "NOMEPCIKUP";
+            tarefaDTO.NumeroPickUp = "123456789";
+            tarefaDTO.NomeDelivery = "NOMEDELIVERY";
+            tarefaDTO.NumeroDelivery = "987654321";
+            tarefaDTO.SalaInicial = "A201";
+            tarefaDTO.SalaFinal = "A202";
+            tarefaDTO.PercursoString = "[cel(a1,1,1),cel(a1,2,2)]";
+
+
+            tarefaRepoMock.Setup(repo => repo.GetTarefasPendentesAsync())
+                .ReturnsAsync(new List<Tarefa>(){tarefa});
+
+            var tarefaService = new TarefaService(tarefaRepoMock.Object, httpClient);
+
+            var controller = new TarefaController(tarefaService);
+
+            var res = await controller.GetTarefasPendentes();
+
+            var okResult = Assert.IsType<OkObjectResult>(res.Result);
+            Assert.Equal(200, okResult.StatusCode);
+            var dtoRes = Assert.IsType<List<TarefaDTO>>(okResult.Value);
+
+            Assert.Equal(tarefaDTO.Id, dtoRes[0].Id);
+            Assert.Equal(tarefaDTO.TipoTarefa, dtoRes[0].TipoTarefa);
+            Assert.Equal(tarefaDTO.CodConfirmacao, dtoRes[0].CodConfirmacao);
+            Assert.Equal(tarefaDTO.DescricaoEntrega, dtoRes[0].DescricaoEntrega);
+            Assert.Equal(tarefaDTO.NomePickUp, dtoRes[0].NomePickUp);
+            Assert.Equal(tarefaDTO.NumeroPickUp, dtoRes[0].NumeroPickUp);
+            Assert.Equal(tarefaDTO.NomeDelivery, dtoRes[0].NomeDelivery);
+            Assert.Equal(tarefaDTO.NumeroDelivery, dtoRes[0].NumeroDelivery);
+            Assert.Equal(tarefaDTO.SalaInicial, dtoRes[0].SalaInicial);
+            Assert.Equal(tarefaDTO.SalaFinal, dtoRes[0].SalaFinal);
+            Assert.Equal(tarefaDTO.PercursoString, dtoRes[0].PercursoString);
+
+        }
+
     }
 }
