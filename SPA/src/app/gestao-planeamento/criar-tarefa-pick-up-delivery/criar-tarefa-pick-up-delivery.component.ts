fc1445a1e7d9ba0b629a643c55ca9a18c1ca96cc
Ezequiel Estima <1211417@isep.ipp.pt>
2023-12-24 16:59:04 +0000

    [US_460.1][Impl] Criação dos componentes criarTarefaVigilancia e criarTarefaPiskUpDelivery e adição de linkagem à sidebar

---
+++ SPA/src/app/gestao-planeamento/criar-tarefa-pick-up-delivery/criar-tarefa-pick-up-delivery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-tarefa-pick-up-delivery',
+  templateUrl: './criar-tarefa-pick-up-delivery.component.html',
+  styleUrls: ['./criar-tarefa-pick-up-delivery.component.css']
+})
+export class CriarTarefaPickUpDeliveryComponent {
+
+}
---
+++ SPA/src/app/gestao-planeamento/criar-tarefa-vigilancia/criar-tarefa-vigilancia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-tarefa-vigilancia',
+  templateUrl: './criar-tarefa-vigilancia.component.html',
+  styleUrls: ['./criar-tarefa-vigilancia.component.css']
+})
+export class CriarTarefaVigilanciaComponent {
+
+}
