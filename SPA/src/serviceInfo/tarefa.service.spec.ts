5aa6e9f6dd06f02e4de1f5a981ee77b5effcd8d1
Ezequiel Estima <1211417@isep.ipp.pt>
2023-12-26 16:26:34 +0000

    [US_460.1][Testes] Adição de testes ao tarefa service

--- SPA/src/serviceInfo/tarefa.service.spec.ts
+++ SPA/src/serviceInfo/tarefa.service.spec.ts
@@ -7,6 +7,8 @@ import { devEnvironment } from 'src/environments/environment.development';
 import { TarefaService } from './tarefa.service';
 import Tarefa from 'src/dataModel/tarefa';
 import AlterarEstadoDaTarefa from 'src/dataModel/alterarEstadoDaTarefa';
+import { CriarVigilancia } from 'src/dataModel/criarVigilancia';
+import { CriarPickUpDelivery } from 'src/dataModel/criarPickUPDelivery';
 
 describe('TarefaService', () => { 
   let service: TarefaService;
@@ -146,5 +148,327 @@ describe('TarefaService', () => {
     expect(putSpy).not.toHaveBeenCalled();
   });
 
+  it('Método criarTarefaVigilancia chama o método POST do HttpClient quando os dados são válidos', () => {
 
+    const testDataInsert = {
+      tipoTarefa: "Vigilancia",
+      nomeVigilancia: "Nome1",
+      numeroVigilancia: "987654321",
+      codEdificio: "A",
+      numeroPiso: 1
+    } as CriarVigilancia;
+
+    const returnPost = {
+      tipoTarefa: "Vigilancia",
+      id: "1",
+      percursoString: "percursoVigilanciaPlaceholder",
+      estadoString: "Pendente",
+      emailRequisitor: "emailPlaceholder",
+      codDispositivo: "",
+      odConfirmacao: null,
+      descricaoEntrega: null,
+      nomePickUp: null,
+      numeroPickUp: null,
+      nomeDelivery: null,
+      numeroDelivery: null,
+      salaInicial: null,
+      salaFinal: null,
+      nomeVigilancia: "Nome1",
+      numeroVigilancia: "987654321",
+      codEdificio: "A",
+      numeroPiso: 1
+      } as unknown as Tarefa;
+
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(returnPost));
+
+    service.criarTarefaVigilancia(testDataInsert.nomeVigilancia, testDataInsert.numeroVigilancia, testDataInsert.codEdificio, testDataInsert.numeroPiso);
+    
+    expect(postSpy).toHaveBeenCalledWith(tarefaUrl,testDataInsert,service.httpOptions);
+  });
+
+  it('Método criarTarefaVigilancia não chama o método POST do HttpClient quando o nome é vazio', () => {
+
+    const testDataInsert = {
+      tipoTarefa: "Vigilancia",
+      nomeVigilancia: "",
+      numeroVigilancia: "987654321",
+      codEdificio: "A",
+      numeroPiso: 1
+    } as CriarVigilancia;
+
+    const returnPost = {
+      tipoTarefa: "Vigilancia",
+      id: "1",
+      percursoString: "percursoVigilanciaPlaceholder",
+      estadoString: "Pendente",
+      emailRequisitor: "emailPlaceholder",
+      codDispositivo: "",
+      odConfirmacao: null,
+      descricaoEntrega: null,
+      nomePickUp: null,
+      numeroPickUp: null,
+      nomeDelivery: null,
+      numeroDelivery: null,
+      salaInicial: null,
+      salaFinal: null,
+      nomeVigilancia: "Nome1",
+      numeroVigilancia: "987654321",
+      codEdificio: "A",
+      numeroPiso: 1
+      } as unknown as Tarefa;
+
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(returnPost));
+
+    service.criarTarefaVigilancia(testDataInsert.nomeVigilancia, testDataInsert.numeroVigilancia, testDataInsert.codEdificio, testDataInsert.numeroPiso);
+    
+    expect(postSpy).not.toHaveBeenCalled();
+  });
+
+  it('Método criarTarefaVigilancia não chama o método POST do HttpClient quando o nome é vazio', () => {
+
+    const testDataInsert = {
+      tipoTarefa: "Vigilancia",
+      nomeVigilancia: "Nome1",
+      numeroVigilancia: "",
+      codEdificio: "A",
+      numeroPiso: 1
+    } as CriarVigilancia;
+
+    const returnPost = {
+      tipoTarefa: "Vigilancia",
+      id: "1",
+      percursoString: "percursoVigilanciaPlaceholder",
+      estadoString: "Pendente",
+      emailRequisitor: "emailPlaceholder",
+      codDispositivo: "",
+      odConfirmacao: null,
+      descricaoEntrega: null,
+      nomePickUp: null,
+      numeroPickUp: null,
+      nomeDelivery: null,
+      numeroDelivery: null,
+      salaInicial: null,
+      salaFinal: null,
+      nomeVigilancia: "Nome1",
+      numeroVigilancia: "987654321",
+      codEdificio: "A",
+      numeroPiso: 1
+      } as unknown as Tarefa;
+
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(returnPost));
+
+    service.criarTarefaVigilancia(testDataInsert.nomeVigilancia, testDataInsert.numeroVigilancia, testDataInsert.codEdificio, testDataInsert.numeroPiso);
+    
+    expect(postSpy).not.toHaveBeenCalled();
+  });
+
+  it('Método criarTarefaPickUpDelivery chama o método POST do HttpClient quando os dados são válidos', () => {
+
+    const testDataInsert = {
+      tipoTarefa: "PickUpDelivery",
+      codConfirmacao: "1234",
+      descricaoEntrega: "DESC",
+      nomePickUp: "ABC",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "123456789",
+      salaInicial: "A203",
+      salaFinal: "A202"
+    } as CriarPickUpDelivery;
+
+    const returnPost = {
+      tipoTarefa: "PickUpDelivery",
+      id: "1",
+      percursoString: "percursoVigilanciaPlaceholder",
+      estadoString: "Pendente",
+      emailRequisitor: "emailPlaceholder",
+      codDispositivo: "",
+      codConfirmacao: "1234",
+      descricaoEntrega: "Desc",
+      nomePickUp: "ABC",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "123456789",
+      salaInicial: "A203",
+      salaFinal: "A202",
+      nomeVigilancia: null,
+      numeroVigilancia: null,
+      codEdificio: null,
+      numeroPiso: null
+    } as unknown as Tarefa;
+
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(returnPost));
+
+    service.criarTarefaPickUpDelivery(testDataInsert.codConfirmacao, testDataInsert.descricaoEntrega, testDataInsert.nomePickUp, testDataInsert.numeroPickUp, testDataInsert.nomeDelivery, testDataInsert.numeroDelivery, testDataInsert.salaInicial, testDataInsert.salaFinal);
+
+    expect(postSpy).toHaveBeenCalledWith(tarefaUrl,testDataInsert,service.httpOptions);
+  });
+
+  it('Método criarTarefaPickUpDelivery não chama o método POST do HttpClient quando o codigo de confimação é vazio', () => {
+
+    const testDataInsert = {
+      tipoTarefa: "PICKupDELIVERY",
+      codConfirmacao: "",
+      descricaoEntrega: "DESC",
+      nomePickUp: "ABC",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "123456789",
+      salaInicial: "A203",
+      salaFinal: "A202"
+    } as CriarPickUpDelivery;
+
+    const returnPost = {
+      tipoTarefa: "PickUpDelivery",
+      id: "1",
+      percursoString: "percursoVigilanciaPlaceholder",
+      estadoString: "Pendente",
+      emailRequisitor: "emailPlaceholder",
+      codDispositivo: "",
+      codConfirmacao: "",
+      descricaoEntrega: "Desc",
+      nomePickUp: "ABC",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "123456789",
+      salaInicial: "A203",
+      salaFinal: "A202",
+      nomeVigilancia: null,
+      numeroVigilancia: null,
+      codEdificio: null,
+      numeroPiso: null
+    } as unknown as Tarefa;
+
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(returnPost));
+
+    service.criarTarefaPickUpDelivery(testDataInsert.codConfirmacao, testDataInsert.descricaoEntrega, testDataInsert.nomePickUp, testDataInsert.numeroPickUp, testDataInsert.nomeDelivery, testDataInsert.numeroDelivery, testDataInsert.salaInicial, testDataInsert.salaFinal);
+
+    expect(postSpy).not.toHaveBeenCalled();
+  });
+
+  it('Método criarTarefaPickUpDelivery não chama o método POST do HttpClient quando a descrição é vazia', () => {
+
+    const testDataInsert = {
+      tipoTarefa: "PICKupDELIVERY",
+      codConfirmacao: "1234",
+      descricaoEntrega: "",
+      nomePickUp: "ABC",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "123456789",
+      salaInicial: "A203",
+      salaFinal: "A202"
+    } as CriarPickUpDelivery;
+
+    const returnPost = {
+      tipoTarefa: "PickUpDelivery",
+      id: "1",
+      percursoString: "percursoVigilanciaPlaceholder",
+      estadoString: "Pendente",
+      emailRequisitor: "emailPlaceholder",
+      codDispositivo: "",
+      codConfirmacao: "1234",
+      descricaoEntrega: "",
+      nomePickUp: "ABC",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "123456789",
+      salaInicial: "A203",
+      salaFinal: "A202",
+      nomeVigilancia: null,
+      numeroVigilancia: null,
+      codEdificio: null,
+      numeroPiso: null
+    } as unknown as Tarefa;
+
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(returnPost));
+
+    service.criarTarefaPickUpDelivery(testDataInsert.codConfirmacao, testDataInsert.descricaoEntrega, testDataInsert.nomePickUp, testDataInsert.numeroPickUp, testDataInsert.nomeDelivery, testDataInsert.numeroDelivery, testDataInsert.salaInicial, testDataInsert.salaFinal);
+
+    expect(postSpy).not.toHaveBeenCalled();
+  });
+
+  it('Método criarTarefaPickUpDelivery chama o método POST do HttpClient quando o nome é vazio', () => {
+
+    const testDataInsert = {
+      tipoTarefa: "PICKupDELIVERY",
+      codConfirmacao: "1234",
+      descricaoEntrega: "DESC",
+      nomePickUp: "",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "123456789",
+      salaInicial: "A203",
+      salaFinal: "A202"
+    } as CriarPickUpDelivery;
+
+    const returnPost = {
+      tipoTarefa: "PickUpDelivery",
+      id: "1",
+      percursoString: "percursoVigilanciaPlaceholder",
+      estadoString: "Pendente",
+      emailRequisitor: "emailPlaceholder",
+      codDispositivo: "",
+      codConfirmacao: "1234",
+      descricaoEntrega: "Desc",
+      nomePickUp: "",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "123456789",
+      salaInicial: "A203",
+      salaFinal: "A202",
+      nomeVigilancia: null,
+      numeroVigilancia: null,
+      codEdificio: null,
+      numeroPiso: null
+    } as unknown as Tarefa;
+
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(returnPost));
+
+    service.criarTarefaPickUpDelivery(testDataInsert.codConfirmacao, testDataInsert.descricaoEntrega, testDataInsert.nomePickUp, testDataInsert.numeroPickUp, testDataInsert.nomeDelivery, testDataInsert.numeroDelivery, testDataInsert.salaInicial, testDataInsert.salaFinal);
+
+    expect(postSpy).not.toHaveBeenCalled();
+  });
+
+  it('Método criarTarefaPickUpDelivery chama o método POST do HttpClient quando o número é vazio', () => {
+
+    const testDataInsert = {
+      tipoTarefa: "PickUpDelivery",
+      codConfirmacao: "1234",
+      descricaoEntrega: "DESC",
+      nomePickUp: "ABC",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "",
+      salaInicial: "A203",
+      salaFinal: "A202"
+    } as CriarPickUpDelivery;
+
+    const returnPost = {
+      tipoTarefa: "PickUpDelivery",
+      id: "1",
+      percursoString: "percursoVigilanciaPlaceholder",
+      estadoString: "Pendente",
+      emailRequisitor: "emailPlaceholder",
+      codDispositivo: "",
+      codConfirmacao: "1234",
+      descricaoEntrega: "Desc",
+      nomePickUp: "ABC",
+      numeroPickUp: "123456789",
+      nomeDelivery: "ABC",
+      numeroDelivery: "",
+      salaInicial: "A203",
+      salaFinal: "A202",
+      nomeVigilancia: null,
+      numeroVigilancia: null,
+      codEdificio: null,
+      numeroPiso: null
+    } as unknown as Tarefa;
+
+    const postSpy = spyOn(httpClient, 'post').and.returnValue(of(returnPost));
+
+    service.criarTarefaPickUpDelivery(testDataInsert.codConfirmacao, testDataInsert.descricaoEntrega, testDataInsert.nomePickUp, testDataInsert.numeroPickUp, testDataInsert.nomeDelivery, testDataInsert.numeroDelivery, testDataInsert.salaInicial, testDataInsert.salaFinal);
+
+    expect(postSpy).not.toHaveBeenCalled();
+  });
 });
