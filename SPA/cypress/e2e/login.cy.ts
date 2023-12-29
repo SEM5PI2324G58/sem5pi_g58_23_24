7636f93861838a0f44504b37837b2ba265388473
Ezequiel Estima <1211417@isep.ipp.pt>
2023-12-29 19:13:03 +0000

    [US_1300.1][Test] Adição do delete do user no testes e2e

--- SPA/cypress/e2e/login.cy.ts
+++ SPA/cypress/e2e/login.cy.ts
@@ -4,22 +4,31 @@ describe('Login', () => {
 
         cy.intercept('POST', '/api/user/signup').as('signup');
         cy.intercept('POST', '/api/user/login').as('login');
+        cy.intercept('DELETE', '/api/user/utente').as('deleteUtente');
 
         //Criar um utilizador para testar o login
+        cy.visit('/login');
+        cy.get('[name="email"]').type('1211469@isep.ipp.pt');
+        cy.get('[name="password"]').type('Password10@');
+        cy.get('button').click();
+        cy.wait('@login')
+
         cy.visit('/registar');
+        cy.get('[name="role"]').select('utente');
         cy.get('[name="name"]').type('UtilizadorTeste');
         cy.get('[name="password"]').type('Password10@');
-        cy.get('[name="email"]').type('utilizadorTeste@isep.ipp.pt');
+        cy.get('[name="email"]').type('utilizadortesteLogin@isep.ipp.pt');
         cy.get('[name="telefone"]').type('912345678');
-        cy.get('[name="role"]').select('gestor de campus');
+        cy.get('[name="nif"]').type('123123123');
         cy.get('button').click();
         cy.wait('@signup')
+        cy.get('[class="logout"]').click();
 
     });
 
     it('login tem sucesso', () => {
         cy.visit('/login');
-        cy.get('[name="email"]').type('utilizadorTeste@isep.ipp.pt');
+        cy.get('[name="email"]').type('utilizadorTesteLogin@isep.ipp.pt');
         cy.get('[name="password"]').type('Password10@');
         cy.get('button').click();
         cy.wait('@login').then((interception) => {
@@ -29,6 +38,10 @@ describe('Login', () => {
     });
 
     afterEach(() => {
-        //TODO apagar utilizador
+        cy.visit('/deleteUtente');
+        cy.get('[name="delete"]').type('delete');
+        cy.get('button').click();
+        cy.wait('@deleteUtente')
+        cy.get('[name="app-message"]').contains("Delete Utente com sucesso!");
     });
 })
