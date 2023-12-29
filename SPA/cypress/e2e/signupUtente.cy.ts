ed2b30d547c985c49c3e2b9178b3e876e0ebb306
1211396 <1211396@isep.ipp.pt>
2023-12-29 17:26:21 +0000

    [US20.1][Testes] Adição dos testes e2e para o caso de sucesso

--- SPA/cypress/e2e/signupUtente.cy.ts
+++ SPA/cypress/e2e/signupUtente.cy.ts
@@ -3,10 +3,13 @@ describe('Signup Utente', () => {
     beforeEach(() => {
         
         cy.intercept('POST', '/api/user/signupUtente').as('signupUtente');
-
+        cy.intercept('DELETE', '/api/user/utente').as('deleteUtente');
+        cy.intercept('PATCH', '/api/user/approveOrReject').as('aprovarUtente');
+        cy.intercept('POST', '/api/user/login').as('login');
+        cy.intercept('GET', '/api/user/listarUtilizadoresPendentes').as('listarUtilizadoresPendentes');
     });
 
-    it('Signup utente input load de pagina', () => {
+    it('Signup utente input load de pagina', () => {    
         cy.visit('/signupUtente')
 
         cy.get('[name="name"]').should('have.attr', 'placeholder', 'Nome *');
@@ -32,6 +35,47 @@ describe('Signup Utente', () => {
         cy.get('button').click();
         cy.get('[name="app-message"]').contains("Deve aceitar os termos e condições.");
  
+    });
+
+    it('Signup tem sucesso', () => {
+        cy.visit('/signupUtente');
+        cy.get('[name="name"]').type('nome');
+        cy.get('[name="email"]').type('emailTesteTesteTeste@isep.ipp.pt');
+        cy.get('[name="telemovel"]').type('913456789');
+        cy.get('[name="nif"]').type('123456789');
+        cy.get('[name="password"]').type('Password10@');
+        cy.get('[id="checkbox"]').check();
+        cy.get('button').click();
+        cy.wait('@signupUtente')
+        cy.get('[name="app-message"]').contains("Conta criada com sucesso!");
+        
+        cy.visit('/login');
+        cy.get('[name="email"]').type('1211469@isep.ipp.pt');
+        cy.get('[name="password"]').type('Password10@');
+        cy.get('button').click();
+        cy.wait('@login')
+
+
+
+        cy.visit('/approveOrRejectUtente')
+        cy.wait('@listarUtilizadoresPendentes')
+        cy.get('[name="selectUser"]').select('emailtestetesteteste@isep.ipp.pt');
+        cy.get('[name="selectEstado"]').select('Aceitar');
+        cy.get('button').click();
+        cy.wait('@aprovarUtente')
+        cy.get('[class="logout"]').click();
+
+        cy.visit('/login');
+        cy.get('[name="email"]').type('emailtestetesteteste@isep.ipp.pt');
+        cy.get('[name="password"]').type('Password10@');
+        cy.get('button').click();
+        cy.wait('@login')
+
+        cy.visit('/deleteUtente');
+        cy.get('[name="delete"]').type('delete');
+        cy.get('button').click();
+        cy.wait('@deleteUtente')
+
     });
     
 
