24d8f190fd3d4e8063ec9e6cc9012c6011121f5a
David Dias <1211415@isep.ipp.pt>
2023-12-30 23:29:12 +0000

    [US1410][Implementation] - Integração entre o servidor prolog, a gestão de pedidos http, a escolha de algoritmos e a execução destes.

--- Planeamento/server.pl
+++ Planeamento/server.pl
@@ -57,6 +57,7 @@ tempo_handler(Request) :-
         http_read_json_dict(Request,Dict,[]),
         process_json(Dict, RobotResults),
         retractall(tempo(_,_,_,_)),
+        retractall(tarefa(_,_,_,_,_,_,_)),
         Robot = RobotResults,
         Algoritmo = Dict.algoritmo,
         iterar_robots(Robot),
@@ -157,6 +158,7 @@ converter_e_assertar(String) :-
 :-dynamic m/4.
 :-dynamic dim_ed/3.
 :-dynamic tempo/4.
+:-dynamic tarefa/7.
 % Dados que vão ser obtidos atraves do pedido da informação do mapa ao MDRI
 % Lista de pisos de cada edifício
 % pisos(IdEdificio,[IdPiso1,IdPiso2,IdPiso3])
@@ -660,8 +662,14 @@ processar_e_assert_resultados([tempos(Tarefa1, Tarefa2, Custo1, Tarefa2Reverso, 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 processar_tarefas_e_assert(ListaTarefas,Nome) :-
+    assertar_tarefas(ListaTarefas),
     processar_tarefas(ListaTarefas, Resultados),
     processar_e_assert_resultados(Resultados,Nome).
+
+assertar_tarefas([]).
+assertar_tarefas([tarefa(Nome,A,B,C,D,E,F)|Resto]) :-
+    assertz(tarefa(Nome,A,B,C,D,E,F)),
+    assertar_tarefas(Resto).
 
 iterar_robots([]).
 iterar_robots([robot(Nome, ListaTarefas) | RestoRobots]) :-
@@ -676,8 +684,7 @@ process_json(Dict, RobotResults) :-
              Term = robot(Name, TaskList)), RobotResults),
     findall(tarefa(Id, N1, N2, P1, N3, N4, P2),
             (member(robot(_, Tasks), RobotResults),
-             member(tarefa(Id, N1, N2, P1, N3, N4, P2), Tasks)), TaskResults),
-    Result = {robots: RobotResults, tasks: TaskResults}.
+             member(tarefa(Id, N1, N2, P1, N3, N4, P2), Tasks)), _).
 
 % ==================================================================================================    
 % =================================Algoritmo Permutações============================================
@@ -689,7 +696,7 @@ melhor_sequencia(Tarefas, MelhorSequencia) :-
 
 avaliar_sequencias([], [], inf). 
 avaliar_sequencias([Sequencia|RestoSequencias], MelhorSequencia, MelhorTempoSequencia) :-
-    avalia(Sequencia, TempoSequencia),
+    avalia1(Sequencia, TempoSequencia),
     avaliar_sequencias(RestoSequencias, TempSequenciaAtual, TempTempoAtual),
     (TempTempoAtual == inf -> 
         MelhorSequencia = Sequencia, MelhorTempoSequencia = TempoSequencia
@@ -699,10 +706,10 @@ avaliar_sequencias([Sequencia|RestoSequencias], MelhorSequencia, MelhorTempoSequ
         MelhorSequencia = TempSequenciaAtual, MelhorTempoSequencia = TempTempoAtual
     ).
 
-avalia([_],0).
+avalia1([_],0).
 
-avalia([Tarefa1,Tarefa2|Resto], TempoIndividuo):-
-    avalia([Tarefa2|Resto], TempoResto),
+avalia1([Tarefa1,Tarefa2|Resto], TempoIndividuo):-
+    avalia1([Tarefa2|Resto], TempoResto),
     Tarefa1 = tarefa(Nome1,_,_,_,_,_,_),
     Tarefa2 = tarefa(Nome2,_,_,_,_,_,_),
     tempo(_,Nome1,Nome2,Custo),
@@ -712,7 +719,324 @@ avalia([Tarefa1,Tarefa2|Resto], TempoIndividuo):-
 % =================================Algoritmo Genético===============================================
 % ==================================================================================================
 
-genetico(_,0).
+genetico_lista_robots([],[]).
+
+genetico_lista_robots([robot(Nome, ListaTarefas) | RestoRobots], [robot(Nome, ListaTarefasFinal) | RestoRobotsFinal]) :-
+    length(ListaTarefas, NumTarefas),
+    NumTarefas > 2,
+    !,
+    genetico(ListaTarefas, ListaTarefasFinal),
+    genetico_lista_robots(RestoRobots, RestoRobotsFinal).
+
+
+%Utiliza a função do david 
+
+genetico_lista_robots([robot(Nome, ListaTarefas) | RestoRobots], [robot(Nome, ListaTarefasFinal) | RestoRobotsFinal]) :-
+    melhor_sequencia(ListaTarefas, ListaTarefasFinal),
+    genetico_lista_robots(RestoRobots, RestoRobotsFinal).
+
+% ==================================================================================================
+
+
+genetico(ListaTarefas,Resultado):-
+    criar_lista_tarefas(ListaTarefas,ListaTarefasFinal),
+    gera(ListaTarefasFinal,ResultadoSemInformacao),
+    obter_tarefas_com_informacao(ResultadoSemInformacao,Resultado).
+
+obter_tarefas_com_informacao([], []).
+
+obter_tarefas_com_informacao([NomeTarefa | Resto], [tarefa(NomeTarefa,A,B,C,D,E,F) | TarefasComInformacao]) :-
+    tarefa(NomeTarefa,A,B,C,D,E,F),
+    obter_tarefas_com_informacao(Resto, TarefasComInformacao).
+
+criar_lista_tarefas([],[]).
+criar_lista_tarefas([tarefa(Nome,_,_,_,_,_,_)|RestoLista],ListaTarefasFinal):-
+    criar_lista_tarefas(RestoLista,ListaTarefasFinal1),
+    append([Nome],ListaTarefasFinal1,ListaTarefasFinal).
+
+inicializa(ListaTarefas):-
+    NG is 10, % Número de gerações
+    DP is 6, % Dimensão da população
+    PC is 0.8, % Probabilidade de cruzamento
+    PM is 0.8, % Probabilidade de mutação
+    PTA is 0.4, % Percentagem do tempo atual
+    (retract(geracoes(_));true), assertz(geracoes(NG)),
+	(retract(populacao(_));true), assertz(populacao(DP)),
+	(retract(prob_cruzamento(_));true), assertz(prob_cruzamento(PC)),
+	(retract(prob_mutacao(_));true), assertz(prob_mutacao(PM)),
+    length(ListaTarefas,NumT),
+    avalia(ListaTarefas,Tempo),
+    TempoAlvo is Tempo * PTA,
+    (retract(tempo_alvo(_));true), assertz(tempo_alvo(TempoAlvo)),
+    (retract(tarefas(_));true), assertz(tarefas(NumT)).
+
+gera(ListaTarefas,Resultado):-
+	inicializa(ListaTarefas),
+	gera_populacao(Pop, ListaTarefas),
+	avalia_populacao(Pop,PopAv),
+	ordena_populacao(PopAv,PopOrd),
+	geracoes(NG),
+	gera_geracao(0,NG,PopOrd,Resultado).
+
+gera_populacao(Pop,ListaTarefas):-
+	populacao(TamPop),
+	tarefas(NumT),
+	gera_populacao(TamPop,ListaTarefas,NumT,Pop).
+
+gera_populacao(0,_,_,[]):-!.
+
+gera_populacao(TamPop,ListaTarefas,NumT,[Ind|Resto]):-
+	TamPop1 is TamPop-1,
+	gera_populacao(TamPop1,ListaTarefas,NumT,Resto),
+	gera_individuo(ListaTarefas,NumT,Ind),
+	not(member(Ind,Resto)).
+gera_populacao(TamPop,ListaTarefas,NumT,L):-
+	gera_populacao(TamPop,ListaTarefas,NumT,L).
+
+gera_individuo([G],1,[G]):-!.
+
+gera_individuo(ListaTarefas,NumT,[G|Resto]):-
+	NumTemp is NumT + 1, % To use with random
+	random(1,NumTemp,N),
+	retira(N,ListaTarefas,G,NovaLista),
+	NumT1 is NumT-1,
+	gera_individuo(NovaLista,NumT1,Resto).
+
+retira(1,[G|Resto],G,Resto).
+retira(N,[G1|Resto],G,[G1|Resto1]):-
+	N1 is N-1,
+	retira(N1,Resto,G,Resto1).
+
+avalia_populacao([],[]).
+avalia_populacao([Ind|Resto],[Ind*V|Resto1]):-
+	avalia(Ind,V),
+	avalia_populacao(Resto,Resto1).
+
+avalia([_],0).
+
+avalia([Tarefa1,Tarefa2|Resto], TempoIndividuo):-
+    avalia([Tarefa2|Resto], TempoResto),
+    tempo(_,Tarefa1,Tarefa2,Custo),
+    TempoIndividuo is TempoResto + Custo.
+
+
+ordena_populacao(PopAv,PopAvOrd):-
+	bsort(PopAv,PopAvOrd).
+
+bsort([X],[X]):-!.
+bsort([X|Xs],Ys):-
+	bsort(Xs,Zs),
+	btroca([X|Zs],Ys).
+
+
+btroca([X],[X]):-!.
+
+btroca([X*VX,Y*VY|L1],[Y*VY|L2]):-
+	VX>VY,!,
+	btroca([X*VX|L1],L2).
+
+btroca([X|L1],[X|L2]):-btroca(L1,L2).
+
+%Caso de paragem chegou ao fim das gerações
+gera_geracao(G,G,Pop, ResultadoFinal):-!,
+    [ResultadoFinal*_ | _] = Pop.
+%Caso de paragem chegou a uma solução com tempo inferior a algo
+gera_geracao(_,_,Pop, ResultadoFinal):- 
+    [ResultadoFinal*Tempo | _] = Pop,
+    tempo_alvo(TempoAlvo),
+    Tempo =< TempoAlvo.
+%Geração Propriamente dita
+gera_geracao(N,G,Pop, ResultadoFinal):-
+	cruzamento(Pop,NPop1), %Faz cruzamento
+	mutacao(NPop1,NPop), %Faz mutação
+	avalia_populacao(NPop,NPopAv), %Avalia a nova população com tempos
+    append(Pop,NPopAv,NAPopAv), %Junta a população antiga com a nova avaliada
+	ordena_populacao(NAPopAv,NAPopOrd), % Ordena por ordem crescente
+    dividir_x_primeiros(2,NAPopOrd,PopMelhores, PopResto), %Obtém os 2 melhores dos pais e dos filhos e os restantes separados
+    populacao(TamPop),
+    QuantPiores is TamPop - 2,
+    escolher_entre_piores(QuantPiores,PopResto,ListaNaoElitista), % Escolhe entre os piores o resto da geração de uma maneira não elitista
+    append(PopMelhores,ListaNaoElitista,PopNova), % Junta os melhores aos não elitistaas
+    ordena_populacao(PopNova,PopFinalOrd), % Ordena-os Produzindo assim o resultado da geração
+	N1 is N+1,
+	gera_geracao(N1,G,PopFinalOrd, ResultadoFinal). % Volta a chamar uma nova geração
+    
+dividir_x_primeiros(0, Resto, [], Resto) :- !.
+
+dividir_x_primeiros(X, [Melhor | Resto], ListaMelhores, RestoLista) :-
+    X > 0,
+    X1 is X - 1,
+    dividir_x_primeiros(X1, Resto, RestoMelhores, RestoLista),
+    ListaMelhores = [Melhor | RestoMelhores].
+
+escolher_entre_piores(NumElementos, ListaPiores, Resultado):-
+    calcular_produto_aleatorio_lista(ListaPiores, ListaPioresComProdutos),
+    sort_por_produtos(ListaPioresComProdutos, ListaComProdutosOrdenada),
+    obter_x_primeiros_s_produto(NumElementos,ListaComProdutosOrdenada, Resultado).
+
+calcular_produto_aleatorio_lista([], []).
+
+calcular_produto_aleatorio_lista([Elemento*Tempo|Resto], [Elemento*Tempo*Produto|RestoComProdutos]):-
+    random(0.0, 1.0, Random),
+    Produto is Tempo * Random,
+    calcular_produto_aleatorio_lista(Resto, RestoComProdutos).
+
+sort_por_produtos(ListaComProdutos, ListaComProdutosOrdenada):-
+    bsortProduto(ListaComProdutos, ListaComProdutosOrdenada).
+
+bsortProduto([X],[X]):-!.
+bsortProduto([X|Xs],Ys):-
+	bsortProduto(Xs,Zs),
+	btrocaProduto([X|Zs],Ys).
+
+
+btrocaProduto([X],[X]):-!.
+
+btrocaProduto([X*VX*AX,Y*VY*AY|L1],[Y*VY*AY|L2]):-
+	AX>AY,!,
+	btrocaProduto([X*VX*AX|L1],L2).
+
+btrocaProduto([X|L1],[X|L2]):-btrocaProduto(L1,L2).
+
+obter_x_primeiros_s_produto(0, _, []) :- !.
+
+obter_x_primeiros_s_produto(X, [Tarefa*Tempo*_ | Resto], Melhores) :-
+    X > 0,
+    X1 is X - 1,
+    obter_x_primeiros_s_produto(X1, Resto, RestantesMelhores),
+    Melhores = [Tarefa*Tempo | RestantesMelhores].
+
+gerar_pontos_cruzamento(P1,P2):-
+	gerar_pontos_cruzamento1(P1,P2).
+
+gerar_pontos_cruzamento1(P1,P2):-
+	tarefas(N),
+	NTemp is N+1,
+	random(1,NTemp,P11),
+	random(1,NTemp,P21),
+	P11\==P21,!,
+	((P11<P21,!,P1=P11,P2=P21);(P1=P21,P2=P11)).
+gerar_pontos_cruzamento1(P1,P2):-
+	gerar_pontos_cruzamento1(P1,P2).
+
+cruzamento(Lista,ListaNova):-
+    random_permutation(Lista,ListaAleatoria), % Faz com que não sejam sempre os pares de tarefas por ordem crescente de valor a serem cruzados
+    cruzamento1(ListaAleatoria,ListaNova).
+
+cruzamento1([],[]).
+cruzamento1([Ind*_],[Ind]).
+cruzamento1([Ind1*_,Ind2*_|Resto],[NInd1,NInd2|Resto1]):-
+	gerar_pontos_cruzamento(P1,P2),
+	prob_cruzamento(Pcruz),random(0.0,1.0,Pc),
+	((Pc =< Pcruz,!,
+        cruzar(Ind1,Ind2,P1,P2,NInd1),
+	  cruzar(Ind2,Ind1,P1,P2,NInd2))
+	;
+	(NInd1=Ind1,NInd2=Ind2)),
+	cruzamento1(Resto,Resto1).
+
+preencheh([],[]).
+
+preencheh([_|R1],[h|R2]):-
+	preencheh(R1,R2).
+
+
+sublista(L1,I1,I2,L):-
+	I1 < I2,!,
+	sublista1(L1,I1,I2,L).
+
+sublista(L1,I1,I2,L):-
+	sublista1(L1,I2,I1,L).
+
+sublista1([X|R1],1,1,[X|H]):-!,
+	preencheh(R1,H).
+
+sublista1([X|R1],1,N2,[X|R2]):-!,
+	N3 is N2 - 1,
+	sublista1(R1,1,N3,R2).
+
+sublista1([_|R1],N1,N2,[h|R2]):-
+	N3 is N1 - 1,
+	N4 is N2 - 1,
+	sublista1(R1,N3,N4,R2).
+
+rotate_right(L,K,L1):-
+	tarefas(N),
+	T is N - K,
+	rr(T,L,L1).
+
+rr(0,L,L):-!.
+
+rr(N,[X|R],R2):-
+	N1 is N - 1,
+	append(R,[X],R1),
+	rr(N1,R1,R2).
+
+
+elimina([],_,[]):-!.
+
+elimina([X|R1],L,[X|R2]):-
+	not(member(X,L)),!,
+	elimina(R1,L,R2).
+
+elimina([_|R1],L,R2):-
+	elimina(R1,L,R2).
+
+insere([],L,_,L):-!.
+insere([X|R],L,N,L2):-
+	tarefas(T),
+	((N>T,!,N1 is N mod T);N1 = N),
+	insere1(X,N1,L,L1),
+	N2 is N + 1,
+	insere(R,L1,N2,L2).
+
+
+insere1(X,1,L,[X|L]):-!.
+insere1(X,N,[Y|L],[Y|L1]):-
+	N1 is N-1,
+	insere1(X,N1,L,L1).
+
+cruzar(Ind1,Ind2,P1,P2,NInd11):-
+	sublista(Ind1,P1,P2,Sub1),
+	tarefas(NumT),
+	R is NumT-P2,
+	rotate_right(Ind2,R,Ind21),
+	elimina(Ind21,Sub1,Sub2),
+	P3 is P2 + 1,
+	insere(Sub2,Sub1,P3,NInd1),
+	eliminah(NInd1,NInd11).
+
+
+eliminah([],[]).
+
+eliminah([h|R1],R2):-!,
+	eliminah(R1,R2).
+
+eliminah([X|R1],[X|R2]):-
+	eliminah(R1,R2).
+
+mutacao([],[]).
+mutacao([Ind|Rest],[NInd|Rest1]):-
+	prob_mutacao(Pmut),
+	random(0.0,1.0,Pm),
+	((Pm < Pmut,!,mutacao1(Ind,NInd));NInd = Ind),
+	mutacao(Rest,Rest1).
+
+mutacao1(Ind,NInd):-
+	gerar_pontos_cruzamento(P1,P2),
+	mutacao22(Ind,P1,P2,NInd).
+
+mutacao22([G1|Ind],1,P2,[G2|NInd]):-
+	!, P21 is P2-1,
+	mutacao23(G1,P21,Ind,G2,NInd).
+mutacao22([G|Ind],P1,P2,[G|NInd]):-
+	P11 is P1-1, P21 is P2-1,
+	mutacao22(Ind,P11,P21,NInd).
+
+mutacao23(G1,1,[G2|Ind],G2,[G1|Ind]):-!.
+mutacao23(G1,P,[G|Ind],G2,[G|NInd]):-
+	P1 is P-1,
+	mutacao23(G1,P1,Ind,G2,NInd).
 
 % ==================================================================================================
 % =================================Gestão dos algoritmos============================================
@@ -725,7 +1049,7 @@ escolher_algoritmo(Robots, Algoritmo, Resultados) :-
 
 escolher_algoritmo(Tarefas, Algoritmo, Resultados) :-
     Algoritmo =:= 0,
-    genetico(Tarefas, Resultados).
+    genetico_lista_robots(Tarefas, Resultados).
 
 % Caso base: quando não há mais robôs para processar
 escolher_algoritmo_robots([], Resultados, Resultados).
