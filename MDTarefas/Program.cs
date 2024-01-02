cd9eff08500984efdcb81e60e1f9db9fef6a0968
David Dias <1211415@isep.ipp.pt>
2024-01-02 20:02:21 +0000

    aceitar pedidos do MDTarefas a partir de qualquer origem.

--- MDTarefas/Program.cs
+++ MDTarefas/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         builder =>
         {
-            builder.WithOrigins("http://localhost:4200")
+            builder.AllowAnyOrigin()
                    .AllowAnyHeader()
                    .AllowAnyMethod();
         });
