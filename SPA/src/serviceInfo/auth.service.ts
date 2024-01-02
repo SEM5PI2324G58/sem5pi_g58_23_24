5c8231d6c6e0fd36c059931815f787ff86c26cc2
Marco Andrade <1211469@isep.ipp.pt>
2024-01-02 12:38:19 +0000

    [US_1310][US_1320] - Impressão de mensagem caso erro 440, 401,403

--- SPA/src/serviceInfo/auth.service.ts
+++ SPA/src/serviceInfo/auth.service.ts
@@ -238,9 +238,23 @@ export class AuthService {
      */
     private handleError<T>(operation = 'operation', result?: T) {
         return (error: any): Observable<T> => {
-            this.log(`${operation} failed: ${error.error}`);
+      
+            if(error.status === 440){
+              this.log("Erro: Sessão expirada.");
+              return of(result as T);
+            }else if(error.status === 401){
+              this.log("Erro: Não está autenticado.");
+              return of(result as T);
+            }else if(error.status === 403){
+              this.log("Erro: Não tem permissões para aceder a este conteúdo.");
+              return of(result as T);
+            }else{
+              console.log("HELLO");
+              this.log(`${operation} falhou: ${error.error}`);
+            }
+      
             return of(result as T);
-        };
+          };
     }
 
     /**
